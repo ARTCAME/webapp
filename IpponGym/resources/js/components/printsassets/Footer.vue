2ec1c40e69ff54d2119e9db8cf67862212d0c3af
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:00 +0200

    Create Footer.vue

---
+++ IpponGym/resources/js/components/printsassets/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-row>
+        <b-col class="sign-ctn">
+            <img
+                class="sign-img-doc"
+                type="image/png"
+                :src="sign">
+            <small class="d-block">Firmado: {{ name }}</small>
+            <small>En Zaragoza a {{ this.$moment().format('dddd, D MMMM YYYY')}}</small>
+        </b-col>
+    </b-row>
+</template>
+<script>
+    export default {
+        props: [
+            'name',
+            'sign',
+        ]
+    }
+</script>
