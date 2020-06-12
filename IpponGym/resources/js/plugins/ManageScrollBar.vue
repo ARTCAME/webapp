cbae8fdf6eaff72259c68d402700a349c03792f0
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:55 +0200

    Create ManageScrollBar.vue

---
+++ IpponGym/resources/js/plugins/ManageScrollBar.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Function to prevent jumps when scroll bar isn't present and shows modal (modal-open adds 15px of padding-right and the added class neutralizes it)
+             */
+            Vue.prototype.$manageScrollBar = function() {
+                const hasScrollBar = document.documentElement.scrollHeight > document.documentElement.clientHeight || document.documentElement.scrollHeight > document.documentElement.innerHeight;
+                if (!hasScrollBar) {
+                    document.body.classList.toggle('neutral-scroll');
+                }
+            }
+        }
+    }
+</script>
