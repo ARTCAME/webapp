56fb81db36f6f54c759c8e10e04e95bd73b58871
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:58 +0200

    Create RandomGradient.vue

---
+++ IpponGym/resources/js/plugins/RandomGradient.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Generates a 2 color to right random gradient
+             *
+             * @param {Object} element: The element to gradient
+             */
+            Vue.prototype.$randomGradient = function(element) {
+                /* Define a gradient not white and black */
+                element.style.background = " linear-gradient(to right, " + '#' + (Math.random()*0xFFFFFF<<0).toString(16) + " " + Math.floor(Math.random() * 25) + "%, " + '#' + (Math.random()*0xFFFFFF<<0).toString(16) +  " " + (Math.floor(Math.random() * 100) + 50) + "%)";
+                element.style.webkitBackgroundClip = "text";
+                element.style.backgroundClip = "text";
+                element.style.webkitTextFillColor = "transparent";
+            }
+        }
+    }
+</script>
