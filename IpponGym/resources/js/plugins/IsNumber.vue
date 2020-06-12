271580b883ba64a0cea2e84c57ecf6372a5e3ede
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:52 +0200

    Create IsNumber.vue

---
+++ IpponGym/resources/js/plugins/IsNumber.vue
@@ -0,0 +1,48 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Function to prevent type/copy on numeric form fields
+             *
+             * @param {Event} event: The typing event
+             */
+            Vue.prototype.$isNumber = function(event) {
+                event = (event) ? event : window.event;
+                if (event.type == 'paste') {
+                    event.preventDefault();
+                    let input = event.clipboardData.getData('text');
+                    input = input.replace(new RegExp('[^0-9]', 'g'), '');
+                    return input;
+                } else {
+                    var charCode = (event.which) ? event.which : event.keyCode;
+                    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                        event.preventDefault();
+                    } else {
+                        return true;
+                    }
+                }
+            }
+            /**
+             * Function to prevent type/copy on decimal form fields
+             *
+             * @param {Event} event: The typing event
+             */
+            Vue.prototype.$isNumberDecimal = function(event) {
+                event = (event) ? event : window.event;
+                if (event.type == 'paste') {
+                    event.preventDefault();
+                    let input = event.clipboardData.getData('text');
+                    input = input.replace(new RegExp('[^0-9,]', 'g'), '');
+                    return input;
+                } else {
+                    var charCode = (event.which) ? event.which : event.keyCode;
+                    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode != 44) {
+                        event.preventDefault();
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+</script>
