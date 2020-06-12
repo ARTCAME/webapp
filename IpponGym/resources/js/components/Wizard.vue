bbcf2ece8b601ea7ed8183996608f6251a445ac8
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:33 +0200

    Create Wizard.vue

---
+++ IpponGym/resources/js/components/Wizard.vue
@@ -0,0 +1,278 @@
+<template>
+    <span>
+        <transition name="wizard-blur-in">
+            <!-- Overlay showed when the wizard has been started -->
+            <div
+                id="wzd-full-face"
+                v-if="wizardLaunched">
+                <b-button-close
+                    @click="stop()"></b-button-close>
+            </div>
+        </transition>
+        <!-- Button launcher always visible on the parent -->
+        <div
+            id="wzd-btn-start"
+            :class="'wzd-launcher' + (wizardLaunched ? ' launched' : '')"
+            @click="$tours[wizardName].start()">
+            <!-- Tooltip out of its target because it needs to be conditionally rendered -->
+            <b-tooltip
+                boundary="window"
+                noninteractive
+                target="wzd-btn-start"
+                v-if="!wizardLaunched">Ayuda</b-tooltip>
+            <span id="wrp-wzdnav-icon">
+                <fa-icon icon="question"></fa-icon>
+            </span>
+        </div>
+        <!-- Invoke vue tour -->
+        <v-tour
+            :callbacks="wzdCallbacks"
+            :name="name"
+            :options="wzdOptions"
+            :steps="steps">
+            <template slot-scope="tour">
+                <transition-group name="wizard-blur-in">
+                    <!-- Progress bar at the bottom of the window -->
+                    <b-progress
+                        height="3px"
+                        id="wrp-wzd-progress-bar"
+                        key="wrp-wzd-progress-bar"
+                        variant="info"
+                        :max="tour.steps.length" >
+                        <b-progress-bar
+                            id="wzd-progress-bar"
+                            :value="tour.currentStep + 1"></b-progress-bar>
+                    </b-progress>
+                    <!-- Custom step -->
+                    <v-step
+                        v-for="(step, index) of tour.steps"
+                        v-if="tour.currentStep === index"
+                        :key="index"
+                        :step="step"
+                        :previous-step="tour.previousStep"
+                        :next-step="tour.nextStep"
+                        :stop="tour.stop"
+                        :is-first="tour.isFirst"
+                        :is-last="tour.isLast"
+                        :labels="tour.labels">
+                        <template>
+                            <div slot="actions">
+                                <!-- Navigation on the current step tooltip -->
+                                <div class="wrp-wzd-nav">
+                                    <button
+                                        class="wzd-nav"
+                                        @click.prevent="tour.isFirst ? (tour.skip(), stop()) : (tour.previousStep(), previousStep(tour))">
+                                        <fa-icon icon="chevron-left"></fa-icon>
+                                    </button>
+                                    <button
+                                        class="wzd-nav"
+                                        @click.prevent="stop(tour)">
+                                        <fa-icon icon="times"></fa-icon>
+                                    </button>
+                                    <button
+                                        class="wzd-nav"
+                                        @click.prevent="tour.isLast ? (tour.finish(), stop()) : (tour.nextStep(), nextStep(tour))">
+                                        <fa-icon
+                                            :icon="tour.isLast ? 'check' : 'chevron-right'"></fa-icon>
+                                    </button>
+                                </div>
+                            </div>
+                        </template>
+                    </v-step>
+                </transition-group>
+            </template>
+        </v-tour>
+    </span>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                currentEl: null, /* Element wizarded */
+                wizardName: null, /* Name of the wizard passed from the parent at name */
+                wizardLaunched: false, /* Flag to determine if the wizard has been launched */
+                wzdCallbacks: {
+                    onStart: this.start,
+                }, /* Callbacks to the vue-tour */
+                wzdOptions: {
+                    useKeyboardNavigation: false,
+                }, /* Options to the vue-tour */
+            }
+        },
+        mounted() {
+            /* Assigning the prop to a local variable */
+            this.$nextTick().then(() => this.wizardName = this.name );
+        },
+        props: [
+            'name', /* String with the name of the wizard started */
+            'steps', /* Array of the steps for vue-tour */
+        ],
+        methods: {
+            /**
+             * Function to acquire the next step element for the vue-tour and to manage the custom class to the next/previous wizarded element that will be change its z-index to show it above the full overlay
+             *
+             * @param Object tour: includes the vue-tour step variables
+             */
+            nextStep(tour) {
+                this.currentEl.classList.remove('wizard-focused');
+                this.currentEl = document.querySelector('[data-v-step="' + this.wizardName + '-' + (tour.currentStep + 1) + '"]');
+                this.currentEl.classList.add('wizard-focused');
+            },
+            /**
+             * Function to acquire the previous step element for the vue-tour and to manage the custom class to the next/previous wizarded element that will be change its z-index to show it above the full overlay
+             *
+             * @param Object tour: includes the vue-tour step variables
+             */
+            previousStep(tour) {
+                this.currentEl.classList.remove('wizard-focused');
+                this.currentEl = document.querySelector('[data-v-step="' + this.wizardName + '-' + (tour.currentStep - 1) + '"]');
+                this.currentEl.classList.add('wizard-focused');
+            },
+            /**
+             * Function that launchs the wizard
+             */
+            start() {
+                this.currentEl = document.querySelector('[data-v-step="' + this.wizardName + '-' + 0 + '"]');
+                this.currentEl.classList.add('wizard-focused');
+                this.wizardLaunched = true;
+            },
+            /**
+             * Stops the wizard
+             */
+            stop() {
+                /* Delaying the time of the rest of the transition to avoid unesthetic z-index change */
+                setTimeout(() => this.currentEl.classList.remove('wizard-focused'), 1000);
+                this.wizardLaunched = false;
+                this.$tours[this.wizardName].stop();
+            },
+        },
+    }
+</script>
+<style>
+    .v-step .v-step__arrow,
+    .v-step .v-step__arrow[data-v-7c9c03f0] {
+        border-color: rgba(255, 255, 255, .95);
+    }
+    .v-step__content {
+        padding: 1rem 1rem 0 1rem!important;
+        text-align: left;
+    }
+    /* Class added with JS to the wizarded element */
+    .wizard-focused {
+        position: relative;
+        z-index: 100;
+    }
+</style>
+<style scoped>
+    .v-step {
+        background-color: rgba(255, 255, 255, .95);
+        border-radius: .25rem;
+        color: rgba(90, 90, 90, 1);
+        filter: drop-shadow(0 0 3px rgba(0,0,0,.5));
+        padding: 0;
+    }
+    .wrp-wzd-nav {
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 7px;
+    }
+    .wzd-launcher {
+        /* background: linear-gradient(217deg, rgba(0, 131, 81,1), rgba(0, 131, 81,0) 70.71%), linear-gradient(127deg, rgba(15, 0, 131,1), rgba(15, 0, 131,0) 70.71%), linear-gradient(336deg, rgba(131, 0, 50,1), rgba(131, 0, 50,0) 70.71%); */
+        background: linear-gradient(65deg, rgba(0, 131, 81, .95) 0, rgba(0, 50, 131, .95) 70%);
+        background-position: 0;
+        background-size: 100%;
+        border-radius: 50%;
+        bottom: 0;
+        box-shadow: 0 0 3px rgba(0,0,0,.5);
+        color: rgba(250,250,250,1);
+        font-size: 30px;
+        height: 50px;
+        left: 30px;
+        line-height: 50px;
+        opacity: .7;
+        overflow: hidden;
+        position: fixed;
+        text-align: center;
+        transform: translate(-10px,-10px);
+        transition: all .3s, background-position 1s;
+        width: 50px;
+        z-index: 999;
+    }
+    .wzd-launcher:hover:not(.launched) {
+        background-position: 100%;
+        opacity: 1;
+        transform: translate(-10px,-10px) scale(1.02);
+    }
+    .wzd-nav {
+        background-size: 600%;
+        color: rgba(250, 250, 250, 1);
+        border: 0;
+        /* border-radius: 50%; */
+        -webkit-flex: 1 1 auto;
+        flex: 1 1 auto;
+        font-size: 20px;
+        height: 25px;
+        line-height: 25px;
+        transition: all .3s, background-position 1s;
+        /* width: 40px; */
+    }
+    .wzd-nav:nth-child(1) {
+        background: linear-gradient(65deg, rgba(0, 50, 131, .95) 1%, rgba(0, 131, 81, .95) 10%);
+        background-position: 0%;
+        background-size: 1000%;
+    }
+    .wzd-nav:nth-child(1):hover {
+        background-position-x: 15%;
+    }
+    .wzd-nav:nth-child(2) {
+        background: rgba(0, 131, 81, .95);
+    }
+    .wzd-nav:nth-child(2) svg {
+        transition: transform .3s;
+    }
+    .wzd-nav:nth-child(2):hover svg {
+        transform: scale(1.3);
+    }
+    .wzd-nav:nth-child(3) {
+        background: linear-gradient(65deg, rgba(0, 131, 81, .95) 50%, rgba(0, 50, 131, .95) 59%);
+        background-size: 1000%;
+        background-position: 55%;
+    }
+    .wzd-nav:nth-child(3):hover {
+        background-position-x: 40%;
+    }
+    #wrp-wzd-progress-bar {
+        background-color: unset!important;
+        border-radius: 0;
+        bottom: 0;
+        left: 0;
+        position: fixed;
+        width: 100%;
+        z-index: 99;
+    }
+    #wrp-wzdnav-icon {
+        color: rgba(250, 250, 250, 1);
+        font-size: 30px;
+        height: 40px;
+        line-height: 40px;
+        width: 40px;
+    }
+    #wzd-full-face {
+        /* background: linear-gradient(to right, rgba(250, 250, 250, .4) 0, rgba(250, 250, 250, .7) 50%, rgba(250, 250, 250, .95) 100%); */
+        background: linear-gradient(180deg, rgba(250, 250, 250, .5) 0%, rgba(250, 250, 250, 1) 70%);
+        display: block;
+        height: 100%;
+        left: 0;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 99;
+    }
+    #wzd-full-face button.close {
+        font-size: 45px;
+    }
+    #wzd-progress-bar {
+        background: linear-gradient(to right, rgba(0, 50, 131, .95) 0, rgba(0, 131, 81, .95) 50%);
+        border-radius: 0!important;
+    }
+</style>
