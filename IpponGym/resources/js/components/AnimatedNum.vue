7817ee7ecc75b866a19003f3e81388c09c8380ec
Arturo <arzzz@hotmail.es>
2020-06-12 09:48:00 +0200

    Create AnimatedNum.vue

---
+++ IpponGym/resources/js/components/AnimatedNum.vue
@@ -0,0 +1,22 @@
+<template>
+    <animated-number
+        class="animated-number"
+        :duration="300"
+        :round="true"
+        :value="this.numFounded"/>
+</template>
+<script>
+    import AnimatedNumber from "animated-number-vue";
+
+    export default {
+        components: {
+            AnimatedNumber
+        },
+        props: [
+            'numFounded'
+        ],
+    };
+</script>
+<style scoped>
+</style>
+
