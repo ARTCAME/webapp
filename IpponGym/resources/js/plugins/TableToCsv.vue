654a2832325b5f6d8837a86b29702af02d3c8f38
Arturo <arzzz@hotmail.es>
2020-06-12 09:50:03 +0200

    Create TableToCsv.vue

---
+++ IpponGym/resources/js/plugins/TableToCsv.vue
@@ -0,0 +1,61 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Function to convert array of table data to a csv file globally
+             *
+             * @param {Array} arrKeys: Contain the strings of the columns of the table that will be added as headers
+             * @param {Array} arrData: Contains the row of the table with the data
+             * @param {String} fileName: Name of the download file
+             * @param {String} elAnimated: On the view we want a feedback to the user, it refers to the flag with shows a button feedback
+             */
+            Vue.prototype.$tableToCsv = function(arrKeys, arrData, fileName, elAnimated = null) {
+                let csvContent, keys;
+                keys = arrKeys;
+                csvContent = '';
+                csvContent += keys.join(';');
+                csvContent += '\n';
+                arrData.forEach(item => {
+                    keys.forEach(key => {
+                        if (item[key]) {
+                            /* Detect if an element is a mongo formatted date and format it */
+                            if (item[key].$date && this.$moment(parseInt(item[key].$date.$numberLong)).isValid()) {
+                                csvContent += this.$moment(parseInt(item[key].$date.$numberLong)).format('DD-MM-YYYY HH:mm:ss');
+                            /* Detect sub objects and fetch it on the same 'cell' */
+                            } else if (item[key] instanceof Object) {
+                                item[key].forEach(subItem => {
+                                    Object.values(subItem).forEach(subItemValue => {
+                                        csvContent += subItemValue + '-';
+                                    })
+                                    csvContent = csvContent.replace(/-$/,'');
+                                    csvContent += ' ';
+                                });
+                            } else {
+                                csvContent += item[key] + ';';
+                            }
+                        /* If the data doesn't has the key insert blank space */
+                        } else {
+                            csvContent += ';';
+                        }
+                    })
+                    csvContent += '\n';
+                })
+                /* Create a dummy element and interact with it to generate the download of the file */
+                var dummyEl = document.createElement('a');
+                dummyEl.href = 'data:text/csv; charset=utf-8,' + encodeURIComponent("\uFEFF" + csvContent);
+                dummyEl.target = '_blank';
+                dummyEl.download = fileName;
+                dummyEl.click();
+                /* Change the flag status to show a feedback to the user on the view */
+                if (elAnimated !== null) {
+                    if (!this[elAnimated]) {
+                        this[elAnimated] = true;
+                    }
+                    setTimeout(() => {
+                        this[elAnimated] = false;
+                    }, 1500);
+                }
+            }
+        }
+    }
+</script>
