c488bca2d090c3d8de1a9c05dc58b667f9a62ed4
Arturo <arzzz@hotmail.es>
2020-06-12 09:48:08 +0200

    Create BottomAlert.vue

---
+++ IpponGym/resources/js/components/BottomAlert.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-alert
+        class="fixed-bottom m-0 position-fixed py-4 rounded-0"
+        dismissible
+        fade
+        id="ls-mutation-alert"
+        style="z-index: 2000"
+        :show="10">
+        {{ content }}
+    </b-alert>
+</template>
+<script>
+export default {
+    props: [
+        'content',
+    ],
+}
+</script>
+<style scoped>
+
+</style>
