9dc250bdde27bdcadac7cb4bff9e1c1c79e00c59
Arturo <arzzz@hotmail.es>
2020-06-12 09:50:00 +0200

    Create ShowToast.vue

---
+++ IpponGym/resources/js/plugins/ShowToast.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Function to show a bootstrap toast globally
+             *
+             * @param {String} variant: The bootstrap variant for the toast
+             * @param {String} text: The content of the toast
+             * @param {String} title: Title of the toast
+             * @param {Integer} delay: The duration on milisecons
+             */
+            Vue.prototype.$showToast = function(variant = 'default', text, title, delay) {
+                this.$bvToast.toast(text, {
+                    autoHideDelay: delay,
+                    title: title,
+                    toaster: 'b-toaster-bottom-center',
+                    variant: variant,
+                });
+            }
+        }
+    }
+</script>
