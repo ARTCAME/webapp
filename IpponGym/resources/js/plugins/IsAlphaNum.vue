b8c020650d0378245a4cadca852a5dab6946c1cd
Arturo <arzzz@hotmail.es>
2020-06-12 09:49:49 +0200

    Create IsAlphaNum.vue

---
+++ IpponGym/resources/js/plugins/IsAlphaNum.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Function to prevent type/copy symbols on alphanumeric field
+             *
+             * @param {Event} event: The typing event
+             */
+            Vue.prototype.$isAlphaNum = function(event) {
+                event = (event) ? event : window.event;
+                if (event.type == 'paste') {
+                    event.preventDefault();
+                    let input = event.clipboardData.getData('text');
+                    input = input.replace(new RegExp('[^A-Za-z0-9]', 'g'), '');
+                    return input;
+                } else {
+                    var charCode = (event.which) ? event.which : event.keyCode;
+                    if ((charCode < 48 || charCode > 57) && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122)) {
+                        event.preventDefault();
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+</script>
