f0f0ae9ed1df465a9b753656a6e512284ec480a7
Arturo <arzzz@hotmail.es>
2020-06-16 12:17:58 +0200

    Create modaldocuments.js

---
+++ IpponGym/resources/js/components/wzdsteps/modaldocuments.js
@@ -0,0 +1,9 @@
+export const wmodaldocuments = [
+    {
+        target: '[data-v-step="wzd-modal-documents-0"]',
+        content: `<h4>Genera y descarga documentos</h4>Con esta herramienta podrás descargar documentos legales o recibos de un socio concreto siempre que dispongamos de su firma digitalizada.<br><br>Una vez seleccionado el socio y según el tipo de documento podrás escoger otros datos (como el intervalo de pago en el caso de un recibo).`,
+        params: {
+            placement: 'top',
+        }
+    },
+]
