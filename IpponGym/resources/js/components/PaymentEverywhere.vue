93f6e87522e8f935f963164de784d8acd115e454
Arturo <arzzz@hotmail.es>
2020-06-16 12:16:44 +0200

    Update PaymentEverywhere.vue

--- IpponGym/resources/js/components/PaymentEverywhere.vue
+++ IpponGym/resources/js/components/PaymentEverywhere.vue
@@ -1,5 +1,12 @@
 <template>
-    <div id="wrp">
+    <div>
+        <!-- Invoke the wizard -->
+        <transition appear name="fade">
+            <wizard
+                name="wzd-modal-payments"
+                v-show="selectedData != null"
+                :steps=wmodalpayments></wizard>
+        </transition>
         <transition mode="out-in" name="fade">
             <!-- Shown only if a customer wasn't selected -->
             <div
@@ -34,28 +41,34 @@
     </div>
 </template>
 <script>
-export default {
-    data() {
-        return {
-            selectedData: null, /* Stores the customer selected wich want to add a new payment */
-            tableFields: [
-                { key: 'active', label: 'Activo', sortable: true, class: 'text-center' },
-                { key: 'customerNumber', label: 'Nº Socio', sortable: true, class: 'text-center' },
-                { key: 'name', label: 'Socio', sortable: true, },
-                { key: 'dni', label: 'DNI', sortable: true, class: 'text-center' },
-                { key: 'use', label: '', sortable: true, },
-            ], /* Table fields */
-        }
-    },
-    methods: {
-        /**
-         * Assing data to a local variable to pass it between components
-         *
-         * @param {Object} data: is the customer with the table row format
-         */
-        selected(data) {
-            this.selectedData = data;
+    import * as WzdSteps from './wzdsteps/modalpayments';
+    export default {
+        data() {
+            return {
+                selectedData: null, /* Stores the customer selected wich want to add a new payment */
+                tableFields: [
+                    { key: 'active', label: 'Activo', sortable: true, class: 'text-center' },
+                    { key: 'customerNumber', label: 'Nº Socio', sortable: true, class: 'text-center' },
+                    { key: 'name', label: 'Socio', sortable: true, },
+                    { key: 'dni', label: 'DNI', sortable: true, class: 'text-center' },
+                    { key: 'use', label: '', sortable: true, },
+                ], /* Table fields */
+                wmodalpayments: null, /* Stores the imported wizard steps */
+            }
+        },
+        created() {
+            /* Initialize the wizard steps content */
+            this.wmodalpayments = WzdSteps.wmodalpayments;
+        },
+        methods: {
+            /**
+             * Assing data to a local variable to pass it between components
+             *
+             * @param {Object} data: is the customer with the table row format
+             */
+            selected(data) {
+                this.selectedData = data;
+            }
         }
     }
-}
 </script>
