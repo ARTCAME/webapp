c9efa3dccd30e9f9813451daad570a9a5d3f40df
Arturo <arzzz@hotmail.es>
2020-06-20 09:28:37 +0200

    Create pagos.js

---
+++ IpponGym/resources/js/tests/pagos.js
@@ -0,0 +1,109 @@
+export const PAGOS = [
+    /* 19/06/20 */
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-0' },
+        { spec: '- Forma de pago: Domiciliación' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-1' },
+        { spec: '- Forma de pago: Efectivo' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-2' },
+        { spec: '- Forma de pago: Tarjeta' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-3' },
+        { spec: '- Forma de pago: Domiciliación' },
+        { spec: '- Impresión de recibo: No' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-4' },
+        { spec: '- Forma de pago: Efectivo' },
+        { spec: '- Impresión de recibo: No' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-5' },
+        { spec: '- Forma de pago: Tarjeta' },
+        { spec: '- Impresión de recibo: No' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-6' },
+        { spec: '- Forma de pago: Domiciliación' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-7' },
+        { spec: '- Forma de pago: Tarjeta' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-8' },
+        { spec: '- Forma de pago: Efectivo' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Añadir un pago', desc: 'Desde cualquier página, usando la herramienta de pagos añade un pago a un socio (a los pagos domiciliados les puedes asignar el nuevo estado que quieras). Deberá tener las siguientes características (usa un socio que ya las tenga)', testName: 'test-pagonew-9' },
+        { spec: '- Forma de pago: Domiciliación' },
+        { spec: '- Impresión de recibo: Sí' },
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago DOMICILIADO desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-0', },
+        { spec: '- Nuevo estado: Pendiente (deberá estar confirmado o devuelto)' },
+        { spec: '- Cambia la forma de pago' }
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago DOMICILIADO desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-1', },
+        { spec: '- Cambia la forma de pago' }
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago EN EFECTIVO desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-2', },
+        { spec: '- Nuevo estado: Pendiente (deberá estar confirmado)' },
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago CON TARJETA desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-3', },
+        { spec: '- Cambia la fecha de confirmación' },
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago EN EFECTIVO desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-4', },
+        { spec: '- Cambia la tarifa (si tenía de karate ponle otra de karate' },
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago CON TARJETA desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-5', },
+        { spec: '- Cambia la tarifa (si tenía de karate ponle otra de karate' },
+    ],
+    [
+        { title: 'Edición de pago desde socio', desc: 'Edita un pago DOMICILIADO desde la página de cualquier socio con almenos un pago', testName: 'test-pagocustomeredit-6', },
+        { spec: '- Cambia la fecha de confirmación (deberá estar confirmado o devuelto' }
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago DOMICILIADO desde la página de pagos', testName: 'test-pagopaymentsedit-0', },
+        { spec: '- Cambia la fecha de confirmación (deberá estar confirmado o devuelto' },
+        { spec: '- Cambia entre estados confirmados (si estaba devuelto ponlo confirmado o al revés' }
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago EN EFECTIVO desde la página de pagos', testName: 'test-pagopaymentsedit-1', },
+        { spec: '- Nuevo estado: Confirmado (deberá estar Pendiente)' },
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago CON TARJETA desde la página de pagos', testName: 'test-pagopaymentsedit-2', },
+        { spec: '- Nuevo estado: Confirmado (deberá estar Pendiente)' },
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago DOMICILIADO desde la página de pagos', testName: 'test-pagopaymentsedit-3', },
+        { spec: '- Nuevo estado: Confirmado (deberá estar Pendiente)' },
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago EN EFECTIVO desde la página de pagos', testName: 'test-pagopaymentsedit-4', },
+        { spec: '- Cambia la fecha de confirmación (deberá estar confirmado o devuelto' },
+        { spec: '- Cambia entre estados confirmados (si estaba devuelto ponlo confirmado o al revés' }
+    ],
+    [
+        { title: 'Edición de pago desde pagos', desc: 'Edita un pago CON TARJETA desde la página de pagos', testName: 'test-pagopaymentsedit-5', },
+        { spec: '- Cambia la fecha de confirmación (deberá estar confirmado o devuelto' },
+        { spec: '- Cambia entre estados confirmados (si estaba devuelto ponlo confirmado o al revés' }
+    ],
+]
