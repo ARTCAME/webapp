c60c4115719f2af46e3a94b775b008366a94fecd
Arturo <arzzz@hotmail.es>
2020-06-20 09:28:45 +0200

    Create cinturones.js

---
+++ IpponGym/resources/js/tests/cinturones.js
@@ -0,0 +1,45 @@
+export const CINTURONES = [
+    /* 19/06/20 */
+    [
+        { title: 'Añadir cinturones desde socio', desc: 'Añade uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-0', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Añadir cinturones desde socio', desc: 'Añade uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-1', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Añadir cinturones desde socio', desc: 'Añade uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-2', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Borrar cinturones desde socio', desc: 'Borra uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-3', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Borrar cinturones desde socio', desc: 'Borra uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-4', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Borrar cinturones desde socio', desc: 'Borra uno o varios cinturones desde la ficha de un socio:', testName: 'test-cinturonescustomer-5', },
+        { spec: '- El socio debe tener tarifa de karate' },
+    ],
+    [
+        { title: 'Añadir cinturones desde cinturones', desc: 'Añade uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-0', },
+    ],
+    [
+        { title: 'Añadir cinturones desde cinturones', desc: 'Añade uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-1', },
+    ],
+    [
+        { title: 'Añadir cinturones desde cinturones', desc: 'Añade uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-2', },
+    ],
+    [
+        { title: 'Borrar cinturones desde cinturones', desc: 'Borra uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-3', },
+    ],
+    [
+        { title: 'Borrar cinturones desde cinturones', desc: 'Borra uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-4', },
+    ],
+    [
+        { title: 'Borrar cinturones desde cinturones', desc: 'Borra uno o varios cinturones desde la página de cinturones:', testName: 'test-cinturonesbelts-5', },
+    ],
+]
