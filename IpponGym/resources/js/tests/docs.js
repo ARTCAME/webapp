33f956859af7a8615f850e4d09d636cc4b88bb8c
Arturo <arzzz@hotmail.es>
2020-06-20 09:28:41 +0200

    Create docs.js

---
+++ IpponGym/resources/js/tests/docs.js
@@ -0,0 +1,38 @@
+export const DOCS = [
+    /* 19/06/20 */
+    [
+        { title: 'Imprimir un recibo', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un recibo (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsbill-0' },
+    ],
+    [
+        { title: 'Imprimir un recibo', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un recibo (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsbill-1' },
+    ],
+    [
+        { title: 'Imprimir un recibo', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un recibo (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsbill-2' },
+    ],
+    [
+        { title: 'Imprimir un recibo', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un recibo (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsbill-3' },
+    ],
+    [
+        { title: 'Imprimir un recibo', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un recibo (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsbill-4' },
+    ],
+    [
+        { title: 'Imprimir autorización de menor', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un documento de autorización de menor (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsunderage-0' },
+        { spec: '- El socio debe tener tutor' },
+    ],
+    [
+        { title: 'Imprimir autorización de menor', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un documento de autorización de menor (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsunderage-1' },
+        { spec: '- El socio debe tener tutor' },
+    ],
+    [
+        { title: 'Imprimir autorización de menor', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un documento de autorización de menor (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsunderage-2' },
+        { spec: '- El socio debe tener tutor' },
+    ],
+    [
+        { title: 'Imprimir autorización de menor', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un documento de autorización de menor (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsunderage-3' },
+        { spec: '- El socio debe tener tutor' },
+    ],
+    [
+        { title: 'Imprimir autorización de menor', desc: 'Desde cualquier página, usando la herramienta de documentos, imprime un documento de autorización de menor (intenta que sea de socios diferentes en cada prueba)', testName: 'test-docsunderage-4' },
+        { spec: '- El socio debe tener tutor' },
+    ],
+]
