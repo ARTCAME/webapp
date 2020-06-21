1e493dc58fd31be9a2d9c206cdefcf956074bef2
Arturo <arzzz@hotmail.es>
2020-06-21 19:56:46 +0200

    Update UsersController.php

--- IpponGym/app/Http/Controllers/Api/UsersController.php
+++ IpponGym/app/Http/Controllers/Api/UsersController.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller {
     /**
-     * Search a customer by field/value provided
+     * Search a user by field/value provided
      *
      * @return User
      */
-    public function search(Request $request)
-    {
+    public function search(Request $request) {
         $field = $request->get('field');
         $value = $request->get('value');
-        return  User::where($field,'=',$value)->get();
+        return  User::where($field, '=', $value)->get();
     }
     /**
      * Registers a new user
@@ -49,5 +50,62 @@ class UsersController extends Controller {
         return response()->json([
             'user' => $user,
             'status' => 'success'], 200);
+    }
+    /**
+     * Get the all the tests by user
+     *
+     * @return Response with the array of the tests
+     */
+    public function getAllTests(Request $request) {
+        try {
+            $users = DB::collection('users')->get();
+            $tests = [];
+            foreach($users as $user) {
+                if (array_key_exists('tests', $user)) {
+                    foreach($user['tests'] as $test) {
+                        array_push($tests, $test);
+                    }
+                }
+            }
+            return $tests;
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+    /**
+     * Get the tests maded by the user
+     *
+     * @return Response with the array of the tests
+     */
+    public function getTests(Request $request) {
+        try {
+            $user = User::where('username', $request[0])->first();
+            return $user['tests'];
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+    /**
+     * Save the tests maded by the user
+     *
+     * @return Response with the array of the tests
+     */
+    public function saveTests(Request $request) {
+        try {
+            $user = User::where('username', $request->username)->first();
+            $user->push('tests', array($request->test));
+            return $user;
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 }
