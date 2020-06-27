6ba8a5f2763679cffbcf7fddcefe9531191fa17c
Arturo <arzzz@hotmail.es>
2020-06-27 20:34:11 +0200

    Create DocUsuarios.vue

---
+++ IpponGym/resources/js/components/userdocassets/DocUsuarios.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="doc-usuarios">
+        <b-form-group>
+            <h1 class="subtitle">
+                Registro de usuarios
+            </h1>
+            <p>
+                El formulario de registro de usuarios nos servirá para dar de alta nuevos usuarios a la aplicación.
+            </p>
+            <p>
+                Los usuarios no pueden estar repetidos y la aplicación nos avisará si estamos intentando dar de alta un usuario existente.
+            </p>
+            <p>
+                El dato más importante del formulario es la selección de rol, existen tres:
+                <br>
+                &emsp;- Usuario: usuario habitual de la aplicación, puede modificar socios, generar pagos y modificar cinturones
+                <br>
+                &emsp;- Administrador: además de lo que puede realizar el rol Usuario, puede generar nuevos usuarios
+                <br>
+                &emsp;- Tester: tiene los mismos derechos que el rol Usuario y además tiene acceso a ver y modificar la sección Tests
+            </p>
+            <p>
+                La selección de rol condiciona qué secciones y apartados puede ver el usuario y en un según qué casos condiciona qué puede hacer y qué no por lo que deberemos escoger correctamente el rol.
+            </p>
+        </b-form-group>
+    </div>
+</template>
