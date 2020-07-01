7b4d50c3ca33345176b1f40b53f785965950293c
Arturo <arzzz@hotmail.es>
2020-07-01 22:01:15 +0200

    Create behavior.js

---
+++ IpponGym/resources/js/tests/behavior.js
@@ -0,0 +1,35 @@
+export const COMPORTAMIENTO = [
+    /* 01/07/20 */
+    [
+        { title: 'Comportamiento general sesión', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-0', },
+        { spec: '- ¿Has podido iniciar sesión correctamente?' },
+        { spec: '- ¿Tus sesiones se han mantenido estables y te has encontrado todo como lo dejaste tras salir de la aplicación?' },
+        { spec: '- ¿Se te ha cerrado la sesión en algún momento? Si es así, ¿has visto algún mensaje de error?' },
+    ],
+    [
+        { title: 'Comportamiento general diseño', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-1', },
+        { spec: '- ¿Cómo valoras el diseño general de la página?' },
+        { spec: '- ¿La estructura de la página (el diseño, la disposición de los elementos) se ha visto comprometida en algún momento?' },
+        { spec: '- ¿Has utilizado diferentes tamaño de pantalla (tablet, móvil, pc) y en todos ellos has visto un diseño y estructura correctos?' },
+    ],
+    [
+        { title: 'Comportamiento general guardando cambios', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-2', },
+        { spec: '- ¿Has notado que a la hora de guardar algún proceso o cambio no respondiera la página o así lo pareciera (detalla, por favor)?' },
+        { spec: '- ¿Se te ha duplicado algún cambio por que al aplicarlo se te ha permitido aplicarlo repetidas veces (detalla, por favor)?' },
+    ],
+    [
+        { title: 'Comportamiento general procesos', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-3', },
+        { spec: '- En general ¿Como valoras el cumplimiento de los procesos y la facilidad para ejecutarlos?' },
+    ],
+    [
+        { title: 'Comportamiento general tutoriales', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-4', },
+        { spec: '- Explica cómo ha ido la ejecución de algún tutorial. Si no hubieras visto ninguno, visita alguna página y ejecuta algún tutorial' },
+        { spec: '¿Has podido seguir los tutoriales correctamente? ¿La navegación ha funcionado bien en todos los casos y has entendido bien el el funcionamiento del tutorial?' },
+        { spec: '- En cuanto al contenido del tutorial, ¿has entendido bien el contenido y te ha servido para comprender bien el proceso que pretendía explicar? Si hubiera habido un problema concreto especifica en qué proceso podemos mejorar' },
+    ],
+    [
+        { title: 'Comportamiento general documentación e información', desc: 'Describe en detalle cómo ha sido tu experiencia con las siguientes cuestiones (es recomendado responder cuando hayas hecho tests sobre procesos):', testName: 'test-comportamiento-5', },
+        { spec: '- ¿Crees que la información incluída en la aplicación ha sido suficiente para entender todos los procesos?' },
+        { spec: '- Explica si añadirías alguna explicación más o más nivel de detalle en algún proceso para ayudar a entenderlo mejor' },
+    ],
+]
