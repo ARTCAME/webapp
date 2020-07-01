d2349a5326658732af278d33e3b39127ab850c06
Arturo <arzzz@hotmail.es>
2020-07-01 22:01:56 +0200

    Create JwtMiddleware.php

---
+++ IpponGym/app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        JWTAuth::parseToken()->authenticate();
+        return $next($request);
+    }
+}
