546fe2147f0b30360a07af2255d5be9a55cb256a
Arturo <arzzz@hotmail.es>
2020-07-01 21:07:35 +0200

    Update MainUsers.vue

--- IpponGym/resources/js/views/MainUsers.vue
+++ IpponGym/resources/js/views/MainUsers.vue
@@ -5,27 +5,69 @@
                 <b-nav-item
                     class="p-0"
                     :active="current == 0"
-                    @click="current = 0; filterTests(''); filter = null;">
+                    @click="current = 0;">
                     Estado de usuarios
                 </b-nav-item>
                 <b-nav-item
                     class="p-0"
                     :active="current == 1"
-                    @click="current = 1; filterTests(''); filter = null;">
+                    @click="current = 1;">
                     Registro de usuario
                 </b-nav-item>
                 <b-nav-item
                     class="p-0"
                     :active="current == 2"
-                    @click="current = 2; filterTests(''); filter = null;">
+                    @click="current = 2;">
                     Cambios de contraseña
                 </b-nav-item>
             </b-nav>
         </template>
-        <transition appear name="blur-in">
+        <transition appear mode="out-in" name="fade">
+            <!-- Status dashboard -->
+            <div
+                key="user-status-dashboard"
+                v-if="current == 0 && authenticatedRole == 'root'">
+                <b-row no-gutters>
+                    <b-input
+                        class="m-2"
+                        placeholder="Filtra los resultados"
+                        @input="filterUsers($event)"></b-input>
+                </b-row>
+                <b-card-group
+                    class="p-2"
+                    columns>
+                    <span
+                        v-if="users && users.length == 0">
+                        No hay usuarios
+                    </span>
+                    <transition-group mode="out-in" name="fade">
+                        <b-card
+                            v-for="user in users"
+                            :key="user._id.$oid">
+                            <b-list-group>
+                                <b-list-group-item
+                                    class="user-data-list"
+                                    v-for="(ud, index) in userData"
+                                    :key="ud + index">
+                                    <b-row>
+                                        <b-col>{{ ud.text }}:</b-col>
+                                        <b-col
+                                            v-if="ud.key == 'tests'">{{ user[ud.key] ? user[ud.key].length : 0 }}</b-col>
+                                        <b-col
+                                            v-else>{{ user[ud.key] }}</b-col>
+                                    </b-row>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </b-card>
+                    </transition-group>
+                </b-card-group>
+            </div>
+            <!-- Register form -->
             <b-form
                 autocomplete="off"
+                key="user-register-form"
                 method="post"
+                v-if="current == 1"
                 @submit.prevent="inRegister">
                 <b-card id="register-form">
                     <b-form-group>
@@ -147,6 +189,59 @@
                     </b-button>
                 </b-card>
             </b-form>
+            <!-- Change password form -->
+            <b-form
+                autocomplete="off"
+                key="user-change-password-form"
+                method="post"
+                v-if="current == 2"
+                @submit.prevent="inUpdatePassword">
+                <b-card id="update-password-form">
+                    <b-form-group>
+                        <h5 md="4" class="subtitle">Cambio de contraseña</h5>
+                    </b-form-group>
+                    <b-form-group label="Nombre de usuario" label-for="upd-username">
+                        <b-form-input
+                            autocomplete="off"
+                            autofocus
+                            id="upd-username"
+                            name="upd-username"
+                            required
+                            type="text"
+                            v-model="updUsername"
+                            v-validate="'required|min:3|existingUsername:' + numUserFounded"
+                            :class="{ 'is-invalid' : errors.has('upd-username') }"
+                            @input="find('username', updUsername)"></b-form-input>
+                        <transition mode="out-in" name="liveFeedbacks">
+                            <b-form-invalid-feedback
+                                v-for="error in errors.collect('upd-username')"
+                                :key="error">
+                                {{ error }}
+                            </b-form-invalid-feedback>
+                        </transition>
+                    </b-form-group>
+                    <b-form-group label="Nueva contraseña" label-for="upd-password">
+                        <b-form-input
+                            id="upd-password"
+                            name="upd-password"
+                            ref="upd-password"
+                            required
+                            v-model="updPassword"
+                            v-validate="{ required: true, min: 3 }"
+                            :class="{ 'is-invalid' : errors.has('upd-password') }"></b-form-input>
+                        <transition mode="out-in" name="liveFeedbacks">
+                            <b-form-invalid-feedback
+                                v-for="error in errors.collect('upd-password')"
+                                :key="error">
+                                {{ error }}
+                            </b-form-invalid-feedback>
+                        </transition>
+                    </b-form-group>
+                    <b-button type="submit">
+                        Actualizar contraseña
+                    </b-button>
+                </b-card>
+            </b-form>
         </transition>
     </b-card>
 </template>
@@ -156,6 +251,7 @@
     export default {
         data() {
             return {
+                auxUsers: [], /* Stores a copy of the users to filter it on search*/
                 current: 0, /* Is the current tab */
                 email: '', /* V-model */
                 name: '', /* V-model */
@@ -172,10 +268,23 @@
                 ], /* Select element options */
                 typingTimer: null, /* Timer for the settimeout */
                 username: '', /* V-model */
+                users: [], /* Users existing on the db */
+                userData: [
+                    { text: 'Nombre', key: 'name' },
+                    { text: 'Nombre de usuario', key: 'username' },
+                    { text: 'E-mail', key: 'email' },
+                    { text: 'Conexiones', key: 'login_count' },
+                    { text: 'Última actividad', key: 'lastActivity' },
+                    { text: 'Última ip', key: 'lastIp' },
+                    { text: 'Último inicio de sesión', key: 'last_login' },
+                    { text: 'Tests completados', key: 'tests' },
+                ], /* Data of the user to show */
+                updUsername: '', /* V-model */
+                updPassword: '', /* V-model */
             }
         },
         computed: {
-            ...mapGetters('auth', ['countUserByEmail', 'countUserByUsername', 'isLoggedIn']),
+            ...mapGetters('auth', ['authenticatedRole', 'isLoggedIn']),
             /**
              * Evaluate if every field is correct
              */
@@ -186,19 +295,33 @@
         methods: {
             ...mapActions('auth', ['register']),
             /**
-             * Finds by email and username the users with api calls to db and assign to local variable the number of users founded
+             * On typing on input, filter the users shown
+             *
+             * @param {String} ev: the value on search input
+             */
+            filterUsers(ev) {
+                this.users = this.auxUsers.filter(user => cleanObj(user).includes(ev.toLowerCase()));
+                /* When the input is clean, restart the shown users */
+                if (ev == '' || ev == null) {
+                    this.users = this.auxUsers;
+                }
+                /* Clean the object stringified data */
+                function cleanObj(value) {
+                    return JSON.stringify(value).toLowerCase().replace(/["{}+]/g,'');
+                }
+            },
+            /**
+             * Finds by email and username the users
              *
              * @param {String} f: the field to search
              * @param {String} v: the value to search
              */
             find(f, v) {
-                clearTimeout(this.typingTimer);
-                this.typingTimer = setTimeout(() => {
-                    http.post('/api/usersearch', { field: f, value: v })
-                        .then((response) => {
-                            f == 'email' ? this.numEmailFounded = response.data.length : this.numUserFounded = response.data.length;
-                        }
-                )}, 500);
+                /* Reset the counters to its initial values */
+                this.numEmailFounded = 0
+                this.numUserFounded = 0;
+                const founded = this.users.filter(user => user[f] == v).length;
+                f == 'email' ? this.numEmailFounded = founded : this.numUserFounded = founded;
             },
             /**
              * Launchs the register action to the db
@@ -211,6 +334,7 @@
                     password: this.password,
                     password_confirmation: this.password_confirmation,
                     role: this.role,
+                    login_count: 0,
                 }
                 http.post('api/register', { ...user })
                     .then(response => {
@@ -221,17 +345,81 @@
                         this.password_confirmation = '';
                         this.role = '';
                         this.$validator.reset();
-                        this.$showToast('success', 'Usuario registrado correctamente', 'Nuevo usuario registrado');
+                        this.$showToast(response.status, response.message, response.title);
                     })
                     .catch(error => {
-                        console.error(error)
+                        this.$showToast('danger', 'No se ha podido crear al nuevo usuario. Código de error: FEMaUs@InRe', 'Ha ocurrido un error');
+                        console.error(error.response ? error.response.data : error);
                     });
             },
+            /**
+             * Update the user requested password
+             */
+            inUpdatePassword() {
+                http.post('api/updatePassword', { newPassword: this.updPassword, id: this.users.filter(user => user.username == this.updUsername)[0]._id.$oid })
+                    .then((response) => {
+                        this.$showToast(response.status, response.message, response.title);
+                    })
+                    .catch(error => {
+                        this.$showToast('danger', 'No se ha podido completar la operación. Código de error: FEMaUs@InUpPa', 'Ha ocurrido un error')
+                        console.error(error.response ? error.response.data : error);
+                    });
+            }
+        },
+        created() {
+            /**
+             * Get all the users registered and assign it as a local variable
+             */
+            http.get('/api/getUsers')
+                .then((response) => {
+                    this.users = response.data;
+                })
+                .catch(error => {
+                    this.$showToast('danger', 'No se han podido obtener los usuarios', 'Ha ocurrido un error')
+                    console.error(error.response ? error.response.data : error);
+                });
+            /**
+             * Get all the created sessions data and assign its data to each user
+             */
+            http.get('/api/getSessions')
+                .then((response) => {
+                    this.users.forEach((user, index) => {
+                        /* Check if a session exists to assign its data to the user */
+                        const session = response.data.filter(session => session.user_id == user._id.$oid);
+                        if (session.length > 0) {
+                            this.$set(this.users[index], 'lastActivity', this.$moment(parseInt(session[0].updated_at.$date.$numberLong)).format('YYYY-MM-DD HH:mm:ss'));
+                            this.$set(this.users[index], 'lastIp', session[0].last_ip);
+                        }
+                    });
+                    /* Save the users to an aux variable to allow the filter of the users */
+                    this.auxUsers = [...this.users];
+                })
+                .catch(error => {
+                    this.$showToast('danger', 'No se han podido obtener las sesiones', 'Ha ocurrido un error')
+                    console.error(error.response ? error.response.data : error);
+                });
         }
     }
 </script>
 <style scoped>
-    #register-form {
+    .user-data-list {
+        padding: .4rem .5rem;
+    }
+    .card-columns {
+        column-count: 3;
+    }
+    #register-form,
+    #update-password-form {
         border-color: transparent!important;
+    }
+    @media screen and (max-width: 1000px) {
+        .card-columns {
+            column-count: 2;
+        }
+    }
+    @media screen and (max-width: 680px) {
+        .card-columns {
+            column-count: 1;
+        }
     }
 </style>
