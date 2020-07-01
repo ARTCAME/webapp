dea22c962f6a59c01653077edad2c2a6c7a42ce5
Arturo <arzzz@hotmail.es>
2020-07-01 22:02:05 +0200

    Update http.js

--- IpponGym/resources/js/utils/http.js
+++ IpponGym/resources/js/utils/http.js
@@ -13,22 +13,18 @@ const http = axios.create({
 
 http.interceptors.response.use(
     response => {
-        console.log('use interceptor http')
         /* The RefreshTokens middleware can send a token on headers, if its present set it as the current token authorization */
         const newtoken = response.headers.authorization;
         if (newtoken) {
-            localStorage.setItem('token', newtoken);
             store.dispatch('auth/setToken', newtoken);
-            console.log('relogin http');
         }
         return response;
     },
     error => {
-        console.log('error interceptor http')
         /* If a login error occurst, close the session */
         if (error.response.status === 401) {
             store.dispatch('auth/logout');
-            // location.reload();
+            location.reload();
         }
         return Promise.reject(error);
 })
