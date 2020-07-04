07d0642bae3ab538ece633b53fbb27be03b28319
Arturo <arzzz@hotmail.es>
2020-07-04 11:03:57 +0200

    Update http.js

--- IpponGym/resources/js/utils/http.js
+++ IpponGym/resources/js/utils/http.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import store from '../store/customers';
 
 // const URL = 'http://127.0.0.1:8090/'
-const URL = 'http://127.0.0.1/'
+// const URL = 'http://127.0.0.1/'
 // const URL = 'http://209.97.131.50/'
+const URL = 'http://127.0.0.1/'
 const http = axios.create({
     baseURL: URL,
     headers: {
