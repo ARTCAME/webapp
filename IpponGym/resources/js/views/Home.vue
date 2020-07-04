540ed5642dff22ab590f03234f862c4c12395d51
Arturo <arzzz@hotmail.es>
2020-07-04 13:03:01 +0200

    Update Home.vue

--- IpponGym/resources/js/views/Home.vue
+++ IpponGym/resources/js/views/Home.vue
@@ -368,14 +368,13 @@ import Axios from 'axios';
                 Axios.get('/api/users').then((response) => console.log(response));
                 Axios.get('api/users').then((response) => console.log(response));
                 Axios.get('Api/users').then((response) => console.log(response));
-                Axios.get('http://127.0.0.1:80/api/users').then((response) => console.log(response));
-                Axios.get('http://127.0.0.1:80/Api/users').then((response) => console.log(response));
-                Axios.get('http://127.0.0.1/api/users').then((response) => console.log(response));
-                Axios.get('http://127.0.0.1/Api/users').then((response) => console.log(response));
+                Axios.get('/users').then((response) => console.log(response));
                 Axios.get('http://209.97.131.50/Api/users').then((response) => console.log(response));
                 Axios.get('http://209.97.131.50/api/users').then((response) => console.log(response));
                 Axios.get('http://209.97.131.50:80/api/users').then((response) => console.log(response));
                 Axios.get('http://209.97.131.50:80/Api/users').then((response) => console.log(response));
+                Axios.get('http://209.97.131.50/users').then((response) => console.log(response));
+                Axios.get('http://209.97.131.50:80/users').then((response) => console.log(response));
 
                 http.post('api/register', { ...user })
                     .then(response => {
