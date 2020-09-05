aaee99ebbec76f329bb505d0db35d49d9b2cce46
Arturo <arzzz@hotmail.es>
2020-09-05 17:35:09 +0200

    Update Footer.vue

--- IpponGym/resources/js/components/printsassets/Footer.vue
+++ IpponGym/resources/js/components/printsassets/Footer.vue
@@ -6,7 +6,7 @@
                 type="image/png"
                 :src="sign">
             <small class="d-block">Firmado: {{ name }}</small>
-            <small>En Zaragoza a {{ this.$moment().format('dddd, D MMMM YYYY')}}</small>
+            <small>En Zaragoza a {{ docDate ? this.$moment(docDate).format('dddd, D MMMM YYYY') : this.$moment().format('dddd, D MMMM YYYY') }}</small>
         </b-col>
     </b-row>
 </template>
@@ -15,6 +15,7 @@
         props: [
             'name',
             'sign',
+            'docDate', /* Will be the date of the accept/decline the document, only for rightsXXX not for bill */
         ]
     }
 </script>
