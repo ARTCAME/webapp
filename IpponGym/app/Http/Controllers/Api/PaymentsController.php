d444a2b93911ce3843c82bcd4b83bf3cad6bd26c
Arturo <arzzz@hotmail.es>
2020-09-08 17:08:46 +0200

    Update PaymentsController.php

--- IpponGym/app/Http/Controllers/Api/PaymentsController.php
+++ IpponGym/app/Http/Controllers/Api/PaymentsController.php
@@ -57,14 +57,18 @@ class PaymentsController extends Controller {
             foreach ($customer['payments'] as $payment) {
                 if ($payment['interval'] == date('Y-m')) {
                     $alreadyGenerated = true;
+                    /* Store the existing payment */
+                    $newPayment = $payment;
                 }
             }
-            /* If doesn't exists a payment registered for this month we create and save it on the customer based on the last payments attributes */
+            /* If doesn't exists a payment registered for this month we create and save it on the customer based on the last payments attributes, here we create all the non manual created monthly payments, later treat the inbank payments to generate the csv bank data */
             if (!$alreadyGenerated) {
                 $newPayment['rate'] = $auxPaymentData['rate'];
                 $newPayment['amount'] = $auxPaymentData['amount'];
                 $newPayment['paymenttype'] = $auxPaymentData['paymenttype'];
                 $newPayment['iban'] = $auxPaymentData['iban'];
+                $newPayment['ibanownername'] = $auxPaymentData['ibanownername'];
+                $newPayment['ibanownerdni'] = $auxPaymentData['ibanownerdni'];
                 $newPayment['interval'] = date('Y-m');
                 $newPayment['status'] = 'Pendiente';
                 $newPayment['dategenerated'] = $date;
@@ -75,23 +79,42 @@ class PaymentsController extends Controller {
                 /* Manage utf8 characters */
                 fprintf($csvoutput, chr(0xEF).chr(0xBB).chr(0xBF));
             }
-            /* Only add to the array of csv file data the bank payments */
-            if ($newPayment['paymenttype'] == 'Domiciliación') {
-                /* If the current iban is used on the array of payments to add to the csv file add the amount to the same payment line because the iban is of the same owner */
-                if (array_search($auxPaymentData['iban'], array_column($csvData, 'iban')) != false) {
-                    (float)$csvData[array_search($auxPaymentData['ibanownerdni'], array_column($csvData, 'dni'))]['amount'] += (float)$auxPaymentData['amount'];
-                /* If the current iban is not used on the array of payments to add to the csv file, add it*/
-                } else {
-                    array_push($csvData, ['name' => $auxPaymentData['ibanownername'], 'dni' => $auxPaymentData['ibanownerdni'], 'iban' => $auxPaymentData['iban'], 'amount' => $auxPaymentData['amount'], 'interval' => $newPayment['interval']]);
+            /* We need to add to the array of the csv data only the inbank payments */
+            if (strcmp($newPayment['paymenttype'], 'Domiciliación') === 0) {
+                /* If the payment is already generated and its status is pending we need to send it to the bank, the already generated and no pending will be ignored because there are already managed too */
+                if ($alreadyGenerated && strcmp($newPayment['status'], 'Pendiente') === 0) {
+                    /* Search on newPayment array which on $alreadyGenerated stores the existing payment */
+                    $searchIban = array_search($newPayment['iban'], array_column($csvData, 'iban'));
+                    /* If don't find the iban on the current csv data, add the already generated payment data to the csv data */
+                    if ($searchIban === false) {
+                        array_push($csvData, ['name' => $newPayment['ibanownername'], 'dni' => $newPayment['ibanownerdni'], 'iban' => $newPayment['iban'], 'amount' => $newPayment['amount'], 'interval' => $newPayment['interval']]);
+                    /* If the iban exists on the current csv data, sum the amounts */
+                    } else {
+                        /* Sum operation, use the dni on the newPayment and csvData to find the index */
+                        (float)$csvData[array_search($newPayment['ibanownerdni'], array_column($csvData, 'dni'))]['amount'] += (float)$auxPaymentData['amount'];
+                    }
+                /* If the payment isn't generated */
+                } else if (!$alreadyGenerated) {
+                    /* Search the payment data saved on the customer profile to determine if exists on the csvData */
+                    $searchIban = array_search($auxPaymentData['iban'], array_column($csvData, 'iban'));
+                    /* If don't find the iban on the current csv data, add the new payment data to the csv data */
+                    if (!$searchIban) {
+                        /* If the iban exists on the current csv data, sum the amounts */
+                        array_push($csvData, ['name' => $auxPaymentData['ibanownername'], 'dni' => $auxPaymentData['ibanownerdni'], 'iban' => $auxPaymentData['iban'], 'amount' => $auxPaymentData['amount'], 'interval' => $newPayment['interval']]);
+                    } else {
+                        /* Sum operation, use the dni on the auxPaymentData (the payment data of the customer profile) and csvData to find the index */
+                        (float)$csvData[array_search($auxPaymentData['ibanownerdni'], array_column($csvData, 'dni'))]['amount'] += (float)$auxPaymentData['amount'];
+                    }
+
                 }
             }
         }
         /* Add the array data to the csv file */
         if (sizeof($csvData) > 0) {
             /* Put the customer/payment data into the csv file */
-            fputcsv($csvoutput, ['Nombre', 'Dni', 'Iban', 'Importe', 'Periodo']);
+            fputcsv($csvoutput, ['Nombre', 'Dni', 'Iban', 'Importe', 'Periodo'], ";");
             foreach ($csvData as $data) {
-                fputcsv($csvoutput, $data);
+                fputcsv($csvoutput, $data, ";");
             }
         }
         /* Send email with the file generated */
