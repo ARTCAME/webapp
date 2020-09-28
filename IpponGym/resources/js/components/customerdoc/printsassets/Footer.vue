e03bfca67213c400ad4ef0cffb3f44a450767467
Arturo <arzzz@hotmail.es>
2020-09-28 11:16:40 +0200

    Create Footer.vue

---
+++ IpponGym/resources/js/components/customerdoc/printsassets/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-row>
+        <b-col class="sign-ctn">
+            <img
+                class="sign-img-doc"
+                type="image/png"
+                :src="sign">
+            <small class="d-block">Firmado: {{ name }}</small>
+            <small>En Zaragoza a {{ docDate ? this.$moment(docDate, 'DD-MM-YYYY HH:MM:ss').format('dddd, D MMMM YYYY') : this.$moment().format('dddd, D MMMM YYYY') }}</small>
+        </b-col>
+    </b-row>
+</template>
+<script>
+    export default {
+        props: [
+            'name',
+            'sign',
+            'docDate', /* Will be the date of the accept/decline the document, only for rightsXXX not for bill */
+        ]
+    }
+</script>
