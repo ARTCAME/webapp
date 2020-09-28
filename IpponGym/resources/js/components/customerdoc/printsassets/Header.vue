8105d7ea29aa5856bb7447aced2e21756f84e0c9
Arturo <arzzz@hotmail.es>
2020-09-28 11:16:42 +0200

    Create Header.vue

---
+++ IpponGym/resources/js/components/customerdoc/printsassets/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <b-row>
+        <b-col class="doc-logo m-auto">
+            <img type="image/png" src="./docslogo.png">
+        </b-col>
+        <b-col class="doc-ippon-data m-auto">
+            Sergio Pérez Silva - 77135997T<br>
+            Leopoldo Romeo 9 50002, Zaragoza
+        </b-col>
+    </b-row>
+</template>
