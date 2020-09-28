4bb06269cd4415b2fcbfe79f6055f0d8a7eeca95
Arturo <arzzz@hotmail.es>
2020-09-28 11:17:22 +0200

    Update router.js

--- IpponGym/resources/js/router/router.js
+++ IpponGym/resources/js/router/router.js
@@ -10,6 +10,7 @@ import NProgress from 'nprogress';
 import Router from 'vue-router';
 import store from '../store/customers';
 import Vue from 'vue';
+
 Vue.use(Router)
 const router = new Router ({
     base: process.env.BASE_URL,
