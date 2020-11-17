50461127a08a53824707f8b8685278188d7d1b35
Arturo <arzzz@hotmail.es>
2020-11-17 11:53:14 +0100

    Update SearchEngine.vue

--- IpponGym/resources/js/components/SearchEngine.vue
+++ IpponGym/resources/js/components/SearchEngine.vue
@@ -9,20 +9,14 @@
                     <b-form-input
                         autocomplete="off"
                         autofocus
+                        debounce="200"
                         data-v-step="wzd-modal-search-0"
                         id="search"
                         name="search"
                         type="search"
-                        v-model="search">
+                        @input="search($event)">
                     </b-form-input>
                 </b-form-group>
-                <TableResults
-                    :fields="tableFields"
-                    :items="searchResult"
-                    :lite="lite"
-                    :pagination="pagination"
-                    @selected="$emit('selected', {...arguments}[0]);"
-                    @choose="$emit('choose', {...arguments}[0])"></TableResults>
             </span>
         </transition>
     </div>
@@ -32,27 +26,38 @@
     export default {
         data() {
             return {
-                search: '', /* The value for the search to make */
             }
         },
         computed: {
             ...mapGetters(['getCustomerByField', 'searchCustomer']),
+        },
+        methods: {
+
             /**
              * Provides the result of the search by the fields _id, name, address, dni, emails, phones on the customers state
              *
              * @return {Array} Array of objects with the items for the table results
              */
-            searchResult() {
-                if (this.search != '') {
-                    return this.idDiscard && this.idDiscard.length > 0 ? this.searchCustomer(this.search).filter(customer => !this.idDiscard.includes(customer._id)) : this.searchCustomer(this.search);
+            search(search) {
+                if (search != '') {
+                    /* Discard the provided ids that are used at other places in the form */
+                    let result =  this.idDiscard && this.idDiscard.length > 0 ? this.searchCustomer(search).filter(customer => !this.idDiscard.includes(customer._id)) : this.searchCustomer(search);
+                    /* Discard the under 18 customer of the results */
+                    if (this.onlyAdult) {
+                        return result.filter(customer => this.$moment().diff(this.$moment(customer.dateofbirth, 'YYYY-MM-DD'), 'years') >= 18);
+                    }
+                    return this.$emit('input', result);
                 }
+                /* Emtpy array if no search has been inserted */
+                return this.$emit('input', []);
             },
         },
         props: [
             'idDiscard', /* Array of Strings with the ids to discard because they are used on other entities at the same form */
-            'lite', /* Boolean from the parent to the child. Is no used here. At the table results hide some elements to make the table more clean */
-            'pagination', /* Integer to apply to the table pagination */
-            'tableFields', /* The table fields to show on the child */
+            // 'lite', /* Boolean from the parent to the child. Is no used here. At the table results hide some elements to make the table more clean */
+            'onlyAdult', /* For search only adults customers (+18) useful to seek a tutors that must be +18 */
+            // 'pagination', /* Integer to apply to the table pagination */
+            // 'tableFields', /* The table fields to show on the child */
         ],
     }
 </script>
