9c709a396379e08ee12f088e3505cefc2ab30aac
Arturo <arzzz@hotmail.es>
2020-11-17 11:52:55 +0100

    Update PhoneBase.vue

--- IpponGym/resources/js/components/PhoneBase.vue
+++ IpponGym/resources/js/components/PhoneBase.vue
@@ -1,44 +1,79 @@
 <template>
-    <transition appear name="fade-height">
-        <div class="wrapper-dismissible">
-            <div class="form-agrupation">
-                <b-btn-close
-                    class="wrapper-dismissible-close"
-                    tabindex="-1"
-                    v-if="!isDisabled"
-                    @click="delFormElement({ _id: form._id, entity: target, entityIndex: contactIndex, field: 'phones', fieldIndex: phoneIndex, })"></b-btn-close>
-                <b-form-row>
-                    <b-form-group class="col-12 col-md-3" label="Teléfono">
-                        <b-form-input
-                            autocomplete="off"
-                            type="tel"
-                            v-model="phone"
-                            v-validate="{ required: true, regex: /^[6-9]{1}\d{8}/, length: 9 }"
-                            :class="{ 'is-invalid' : errors.has(validateName) }"
-                            :disabled="isDisabled"
-                            :name="validateName"
-                            @drop.prevent
-                            @keypress="$isNumber($event)"
-                            @paste="phone = $isNumber($event)"></b-form-input>
-                        <transition mode="out-in" name="liveFeedbacks">
-                            <b-form-invalid-feedback
-                                v-if="errors.has(validateName)">
-                                {{ errors.first(validateName) }}
-                            </b-form-invalid-feedback>
-                        </transition>
-                    </b-form-group>
-                    <b-form-group class="col-12 col-md-9" label="Notas">
-                        <b-form-input
-                            autocomplete="off"
-                            placeholder="Si es del trabajo, de casa, horario o persona de contacto, etc"
-                            type="text"
-                            v-model="notes"
-                            :disabled="isDisabled"></b-form-input>
-                    </b-form-group>
-                </b-form-row>
+    <div>
+        <TransitionExpand>
+            <div
+                key="form-warning-phones"
+                v-if="!inPhones || (inPhones && inPhones.length == 0)">
+                <b-alert
+                    class="py-1"
+                    show
+                    variant="info">
+                    No has añadido ningún teléfono y es muy recomendable que el {{ target == 'customer' ? 'socio' : target == 'tutor' ? 'tutor' : 'contacto'}} tenga por lo menos uno
+                </b-alert>
             </div>
-        </div>
-    </transition>
+        </TransitionExpand>
+        <TransitionExpand group>
+            <div
+                class="expandable"
+                v-for="(phone, phoneIndex) in inPhones"
+                :key="target + '_phone_' + phoneIndex">
+                <div class="wrapper-dismissible">
+                    <div class="form-agrupation">
+                        <b-btn-close
+                            class="wrapper-dismissible-close"
+                            tabindex="-1"
+                            v-if="!isDisabled"
+                            @click="delFormElement({ _id: form._id, entity: target, entityIndex: targetIndex, field: 'phones', fieldIndex: phoneIndex, })"></b-btn-close>
+                        <b-form-row>
+                            <b-form-group class="col-12 col-md-3" label="Teléfono">
+                                <b-form-input
+                                    autocomplete="off"
+                                    debounce="500"
+                                    type="tel"
+                                    v-validate="{ required: true, regex: /^[6-9]{1}\d{8}/, length: 9 }"
+                                    :class="{ 'is-invalid' : errors.has(validateName(phoneIndex)) }"
+                                    :disabled="isDisabled"
+                                    :name="validateName(phoneIndex)"
+                                    :value="phone.phone"
+                                    @drop.prevent
+                                    @keypress="$isNumber($event)"
+                                    @paste="$emit('input', { newVal: { phone: $isNumber($event), notes: phone.notes }, arrayIndex: phoneIndex, targetIndex: targetIndex })"
+                                    @input="$emit('input', { newVal: { phone: $event, notes: phone.notes }, arrayIndex: phoneIndex, targetIndex: targetIndex })"></b-form-input>
+                                <transition mode="out-in" name="liveFeedbacks">
+                                    <b-form-invalid-feedback
+                                        v-if="errors.has(validateName(phoneIndex))">
+                                        {{ errors.first(validateName(phoneIndex)) }}
+                                    </b-form-invalid-feedback>
+                                </transition>
+                            </b-form-group>
+                            <b-form-group class="col-12 col-md-9" label="Notas">
+                                <b-form-input
+                                    autocomplete="off"
+                                    debounce="500"
+                                    placeholder="Si es del trabajo, de casa, horario o persona de contacto, etc"
+                                    type="text"
+                                    :disabled="isDisabled"
+                                    :value="phone.notes"
+                                    @input="$emit('input', { newVal: { phone: phone.phone, notes: $event }, arrayIndex: phoneIndex, targetIndex: targetIndex })"></b-form-input>
+                            </b-form-group>
+                        </b-form-row>
+                    </div>
+                </div>
+            </div>
+        </TransitionExpand>
+        <b-row
+            class="mb-4"
+            no-gutters
+            v-if="!isDisabled">
+            <b-button
+                variant="ig-gradient"
+                :class="'ig-add-button ml-auto' + (!inPhones || (inPhones && inPhones.length == 0) ? ' expanded' : '')"
+                @click="addNewElement({ _id: form._id, element: 'phones', entity: target, entityIndex: targetIndex })">
+                <fa-icon class="mr-3" icon="plus"></fa-icon>
+                Añadir teléfono
+            </b-button>
+        </b-row>
+    </div>
 </template>
 <script>
 import { mapActions, mapState } from 'vuex';
@@ -56,49 +91,26 @@ export default {
         form() {
             return this.$route.name == 'customers.new' ? this.newCustomerForm : this.editCustomerForm;
         },
-        /**
-         * v-model for the phone notes
-         */
-        notes: {
-            get() {
-                return this.inPhone.notes ? this.inPhone.notes : '';
-            },
-            set(val) {
-                this.$emit('input', { phone: this.phone, notes: val });
-            }
-        },
-        /**
-         * v-model for the phone
-         */
-        phone: {
-            get() {
-                return this.inPhone.phone ? this.inPhone.phone : ''
-            },
-            set(val) {
-                this.$emit('input', { phone: val, notes: this.notes });
-            }
-        },
-        /**
-         * Returns a name builded based on the target to use it at the validation process
-         *
-         * @return {String} The name to use at the validator
-         */
-        validateName() {
-            return this.target + (this.target == 'contacts' ? '-' + this.contactIndex : '') + '-phone-' + this.phoneIndex;
-        },
     },
     inject: [
         '$validator', /* Inject the main $validator from the parent */
     ],
     methods: {
-        ...mapActions(['delFormElement']),
+        ...mapActions(['addNewElement', 'delFormElement']),
+        /**
+         * Returns a name builded based on the target to use it at the validation process
+         *
+         * @return {String} The name to use at the validator
+         */
+        validateName(phoneIndex) {
+            return this.target + (this.targetIndex ? '-' + this.targetIndex : '') + '-phone-' + phoneIndex;
+        },
     },
     props: [
-        'contactIndex', /* When the parent is a contact will receive this index */
         'isDisabled', /* Boolean that indicates if the form is being edited */
-        'inPhone', /* The phone data to render */
-        'phoneIndex', /* The phone index on the array of phones of the parent */
+        'inPhones', /* The phone data to render */
         'target', /* The parent (customer, tutor or contact) */
+        'targetIndex', /* When the parent is an array of parents (ie contacts) will receive its index */
     ],
 }
 </script>
