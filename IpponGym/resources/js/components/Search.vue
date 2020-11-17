d903cd0f084ce420b4d4fc4539f3f823b361c469
Arturo <arzzz@hotmail.es>
2020-11-17 11:53:01 +0100

    Update Search.vue

--- IpponGym/resources/js/components/Search.vue
+++ IpponGym/resources/js/components/Search.vue
@@ -1,30 +1,53 @@
 <template>
-        <div>
-            <!-- Invoke the wizard -->
-            <wizard
-                name="wzd-modal-search"
-                :steps=wmodalsearch></wizard>
-            <b-form-group class="mb-0">
-                <h5 class="subtitle subtitle-sub">Busca al socio</h5>
-            </b-form-group>
-            <SearchEngine
-                :pagination="15"
-                :tableFields="tableFields"
-                @selected="selected(...arguments)"></SearchEngine>
-        </div>
+    <div class="modal-asset-container">
+        <!-- Invoke the wizard -->
+        <wizard
+            name="wzd-modal-search"
+            :steps=wmodalsearch></wizard>
+        <b-form-group class="mb-0">
+            <h5 class="subtitle subtitle-sub">Busca al socio</h5>
+        </b-form-group>
+        <SearchEngine
+            @input="searchResult = $event"></SearchEngine>
+        <TableResults
+            :clicableRows="true"
+            :fields="tableFields"
+            :items="searchResult"
+            :pagination="15"
+            @row-clicked="selected(...arguments)">
+            <template
+                #actions="row">
+                <b-button
+                    class="ig-small-btn"
+                    size="sm"
+                    variant="outline-primary"
+                    @click="selected(row.row.item)">
+                    <span class="text">Ir a la ficha</span>
+                </b-button>
+            </template>
+            <template
+                #col="fields">
+                <col
+                    v-for="field in fields.fields"
+                    :key="field.key"
+                    :style="{ width:  field.key == 'active' ? '60px' : field.key == 'dni' ? '110px' : field.key == 'customerNumber' ? '75px' : field.key == 'actions' ? '100px' : 'auto' }">
+            </template>
+        </TableResults>
+    </div>
 </template>
 <script>
     import * as WzdSteps from './wzdsteps/modalsearch';
     export default {
         data() {
             return {
+                searchResult: null, /* The result of the search */
                 tableFields: [
                     { key: 'active', label: 'Activo', sortable: true, class: 'text-center' },
                     { key: 'customerNumber', label: 'Nº Socio', sortable: true, class: 'text-center' },
-                    { key: 'name', label: 'Socio', sortable: true, },
+                    { key: 'name', label: 'Socio', sortable: true, class: 'text-nowrap'},
                     { key: 'phones', label: 'Teléfonos', sortable: true, },
                     { key: 'dni', label: 'DNI', sortable: true, class: 'text-center' },
-                    { key: 'enter', label: '', },
+                    { key: 'actions', label: '', class: 'text-center' },
                 ], /* Table fields to the search engine results table */
                 wmodalsearch: null, /* Stores the imported wizard steps */
             }
@@ -45,10 +68,9 @@
                     if (this.$route.path == ('/socio/' + customer._id) || this.$route.path == ('/socio/' + customer._id + '/editar')) {
                         this.$showToast('warning', 'Ya estás en el socio que quieres consultar', 'Aviso', 5000);
                     } else if (this.$route.name == 'customers.edit') {
-                        let result = confirm('No has guardado los cambios, ¿seguro que quieres salir?');
-                        if (result) {
-                            return this.$router.push({ path: '/socio/' + customer._id });
-                        }
+                        /* Hide the search modal */
+                        this.$bvModal.hide('search-nav-modal')
+                        return this.$router.push({ path: '/socio/' + customer._id });
                     } else {
                         this.$router.push({ path: '/socio/' + customer._id });
                         /* Hide the search modal */
