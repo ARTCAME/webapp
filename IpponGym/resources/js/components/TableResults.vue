cce1d31154331c7d0a3305fb8908bdbc0229140f
Arturo <arzzz@hotmail.es>
2020-11-17 11:53:18 +0100

    Update TableResults.vue

--- IpponGym/resources/js/components/TableResults.vue
+++ IpponGym/resources/js/components/TableResults.vue
@@ -3,6 +3,7 @@
         borderless
         class="mb-0"
         empty-text="Ningún socio coincide con estos criterios"
+        fixed
         ref="searchTable"
         responsive
         select-mode="single"
@@ -11,13 +12,27 @@
         :current-page="searchTablePagination"
         :fields="fields"
         :items="items"
-        :per-page="pagination ? pagination : 5">
+        :per-page="pagination"
+        :tbody-tr-class="clicableRows ? 'clicableRow' : ''"
+        @row-clicked="clicableRows ? $emit('row-clicked', $event) : ''">
+        <template
+            #cell(type)="row">
+            {{ row.value == 'periodic' ? 'Periódico' : 'Manual'}}
+        </template>
         <template
             #cell(active)="row">
             <b-form-checkbox
                 disabled
                 switch
                 v-model="row.value"></b-form-checkbox>
+        </template>
+        <template
+            #cell(name)="row">
+            <div
+                class="pl-1 table-text-overflowed text-nowrap"
+                v-overflown="row.value">
+                {{ row.value }}
+            </div>
         </template>
         <template
             #cell(phones)="row">
@@ -28,87 +43,39 @@
             </span>
         </template>
         <template
+            #cell(dni)="row">
+            {{ row.value != '' && row.value != null ? row.value : row.item.tutor ? row.item.tutor.dni + '(tutor)' : 'No disponible' }}
+        </template>
+        <template
+            #cell(actions)="row">
+            <slot
+                name="actions"
+                :row="row">
+                <b-button
+                    class="ig-small-btn"
+                    size="sm"
+                    variant="outline-primary"
+                    @click="$emit('choose', row.item)">
+                    <span class="text">Usar</span>
+                </b-button>
+            </slot>
+        </template>
+        <template
             #table-caption
-            v-if="!lite && (($refs.searchTable && $refs.searchTable.filteredItems.length > 5) || (items && items.length > 5))">
+            v-if="!lite && (($refs.searchTable && $refs.searchTable.filteredItems.length > pagination) || (items && items.length > pagination))">
             <b-row align-h="end" no-gutters>
                 <b-pagination
                     aria-controls="searchTable"
                     class="float-right mb-0"
                     size="sm"
                     v-model="searchTablePagination"
-                    :per-page="pagination ? pagination : 5"
+                    :per-page="pagination"
                     :total-rows="$refs.searchTable ? $refs.searchTable.filteredItems.length : items ? items.length : 0"></b-pagination>
             </b-row>
         </template>
         <template
-            #cell(use)="row"
-            v-if="!customer">
-            <b-button
-                class="ig-modal-table-btn"
-                size="sm"
-                variant="outline-primary"
-                @click="$emit('choose', row.item)">
-                Usar
-            </b-button>
-        </template>
-        <template
-            #cell(see)="row">
-            <b-link
-                class="btn btn-outline-secondary btn-sm ig-modal-table-btn ig-modal-table-btn-svg unformated-link"
-                target="_blank"
-                title="Abre la ficha del socio"
-                v-b-tooltip.top.hover.noninteractive
-                :to="{ name: 'customers.profile', params: { id: row.item._id } }">
-                <fa-icon icon="eye"></fa-icon>
-            </b-link>
-        </template>
-        <template
-            #cell(enter)="row">
-            <b-link
-                class="btn btn-outline-success btn-sm ig-modal-table-btn ig-modal-table-btn-svg unformated-link"
-                positioning="top"
-                title="Accede a la ficha del socio"
-                v-b-tooltip.hover.noninteractive
-                :to="{ name: 'customers.profile', params: { id: row.item._id } }"
-                @click="$bvModal.hide('search-nav-modal')">
-                <!-- @click="$emit('selected', row.item)"> -->
-                <fa-icon class="text-success" icon="eye"></fa-icon>
-            </b-link>
-        </template>
-        <!-- In a specific search page we can include some of these other columns to the table
-        <template
-            #cell(emails)="row">
-            <span
-                v-for="email in row.value"
-                :key="email">
-                {{ email }}<br>
-            </span>
-        </template> -->
-        <!-- <template
-            #cell(name)="row">
-            <b-link
-                positioning="top"
-                target="_blank"
-                title="Abre la ficha del socio"
-                v-b-tooltip.hover.noninteractive
-                v-html="row.value"
-                :to="{ name: 'socios.editar', params: { id: row.item._id } }"></b-link>
-        </template> -->
-        <!-- <template
-            #cell(phones)="row">
-            <span
-                v-for="phoneGroup in row.value"
-                :key="phoneGroup.phone">
-                {{ phoneGroup.phone }}<br>
-            </span>
-        </template> -->
-        <template
             #table-colgroup="scope">
-            {{ scope }}
-            <col
-                v-for="field in scope.fields"
-                :key="field.key"
-                :style="{ width: field.key == 'active' || field.key == 'enter' || field.key == 'see' || field.key == 'use' ? '30px' : field.key == 'dni' ? '110px' : field.key == 'customerNumber' ? '75px' : 'auto' }">
+            <slot name="col" v-bind:fields="scope.fields"></slot>
         </template>
     </b-table>
 </template>
@@ -119,20 +86,38 @@ export default {
             searchTablePagination: 1, /* Marks the current page, always starts at the first */
         }
     },
-    props: [
-        'customer', /* Boolean to determine if the parent (or grand-parent) is a customer and hide some elements */
-        'fields', /* The table fields. Depends of the parent */
-        'items', /* The table items, passeds from the parent */
-        'lite', /* Flag to hide some elements depending of the parent needs */
-        'pagination', /* Integer to apply on pagination */
-    ],
+    created() {
+        /* Force the update of the component to recalculate the directive that adds the title to the name on every row */
+        window.addEventListener('resize', () => { this.$forceUpdate() });
+    },
+    destroyed() {
+        window.removeEventListener('resize', () => { this.$forceUpdate() });
+    },
+    props: {
+        clicableRows: Boolean, /* Boolean to enable the click to enter on every table row */
+        customer: Object, /* Boolean to determine if the parent (or grand-parent) is a customer and hide some elements */
+        fields: Array, /* The table fields */
+        items: Array, /* The table items, passeds from the parent */
+        lite: Boolean, /* Flag to hide some elements depending of the parent requirements */
+        pagination: {
+            type: Number,
+            default: 5,
+        }, /* Integer to apply on pagination, if its 0 the pagination is not enabled */
+    }
 }
 </script>
-<style scoped>
+<style>
+    .clicableRow {
+        cursor: pointer;
+    }
+    .ig-small-btn {
+        line-height: 22px;
+    }
     .ig-modal-table-btn-svg {
         line-height: 18px;
     }
     /* Sets like a button behaviour */
+    .ig-small-btn:hover .text,
     .ig-modal-table-btn:hover svg {
         color: rgb(248, 249, 250)!important;
         transition: .35s;
