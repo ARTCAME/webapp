264930225e2d3af53240af2fd0250551f0790ad9
Arturo <arzzz@hotmail.es>
2020-11-17 11:54:11 +0100

    Create ChangeText.vue

---
+++ IpponGym/resources/js/plugins/ChangeText.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Change the text of an element
+             *
+             * @param {Array} id: The id of the elements to change
+             */
+            Vue.prototype.$ChangeText = function(id, newText, ms) {
+                const elem = document.getElementById(id);
+                const prevText = elem.innerText;
+                elem.innerText = newText;
+                setTimeout(() => {
+                    elem.innerText = prevText;
+                }, ms)
+            }
+        }
+    }
+</script>
