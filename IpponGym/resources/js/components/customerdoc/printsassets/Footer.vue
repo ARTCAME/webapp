007a3a6e72c0c6c6d995df879b2270c5a13910d1
Arturo <arzzz@hotmail.es>
2020-11-17 11:51:53 +0100

    Update Footer.vue

--- IpponGym/resources/js/components/customerdoc/printsassets/Footer.vue
+++ IpponGym/resources/js/components/customerdoc/printsassets/Footer.vue
@@ -1,9 +1,15 @@
 <template>
     <b-row>
         <b-col class="sign-ctn">
+            <div
+                class="m-0 p-0 sign-img-doc"
+                v-if="!sign">
+                <small>Firma no disponible</small>
+            </div>
             <img
                 class="sign-img-doc"
                 type="image/png"
+                v-else
                 :src="sign">
             <small class="d-block">Firmado: {{ name }}</small>
             <small>En Zaragoza a {{ docDate ? this.$moment(docDate, 'DD-MM-YYYY HH:MM:ss').format('dddd, D MMMM YYYY') : this.$moment().format('dddd, D MMMM YYYY') }}</small>
@@ -19,3 +25,13 @@
         ]
     }
 </script>
+<style scoped>
+    .sign-ctn {
+        text-align: end;
+    }
+    .sign-img-doc {
+        height: 105px;
+        min-height: 105px;
+        width: auto;
+    }
+</style>
