6fe773c7cb645f64946a3dca9d114f9a7aed9090
Arturo <arzzz@hotmail.es>
2020-11-17 11:54:16 +0100

    Create giveShortFocus.vue

---
+++ IpponGym/resources/js/plugins/giveShortFocus.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Give a class to show focus on a specified element
+             *
+             * @param {Array} ids: The ids of the elements to focus
+             */
+            Vue.prototype.$giveShortFocus = function(ids) {
+                ids.forEach(id => {
+                    const list = document.getElementById(id).classList;
+                    if (list.contains('short-focus')) {
+                        setTimeout(() => {
+                            document.getElementById(id).classList.remove('short-focus');
+                        }, 1000);
+                    } else {
+                        list.add('short-focus');
+                        setTimeout(() => {
+                            document.getElementById(id).classList.remove('short-focus');
+                        }, 1000);
+                    }
+                })
+            }
+        }
+    }
+</script>
