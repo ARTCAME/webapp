a33d8f1df2bdca04361ec38d33a290e93dae6836
Arturo <arzzz@hotmail.es>
2020-11-17 11:52:44 +0100

    Update PaymentEverywhere.vue

--- IpponGym/resources/js/components/PaymentEverywhere.vue
+++ IpponGym/resources/js/components/PaymentEverywhere.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <!-- Invoke the wizard -->
+    <div class="modal-asset-container">
+        <!-- Invoke the wizard
         <transition appear name="fade">
             <wizard
                 name="wzd-modal-payments"
                 v-show="selectedData != null"
                 :steps=wmodalpayments></wizard>
-        </transition>
+        </transition> -->
         <transition mode="out-in" name="fade">
             <!-- Shown only if a customer wasn't selected -->
             <div
@@ -16,12 +16,35 @@
                     <h5 class="subtitle subtitle-sub">Busca al socio y seleccionalo en la tabla</h5>
                 </b-form-group>
                 <SearchEngine
+                    @input="searchResult = $event"></SearchEngine>
+                <TableResults
+                    :clicableRows="true"
+                    :fields="tableFields"
+                    :items="searchResult"
                     :pagination="15"
-                    :tableFields="tableFields"
-                    @choose="selected(...arguments)"></SearchEngine>
+                    @row-clicked="selected(...arguments)">
+                    <template
+                        #actions="row">
+                        <b-button
+                            class="ig-small-btn"
+                            size="sm"
+                            variant="outline-primary"
+                            @click="selected(row.row.item)">
+                            <span class="text">Usar</span>
+                        </b-button>
+                    </template>
+                    <template
+                        #col="fields">
+                        <col
+                            v-for="field in fields.fields"
+                            :key="field.key"
+                            :style="{ width:  field.key == 'active' ? '60px' : field.key == 'dni' ? '110px' : field.key == 'customerNumber' ? '75px' : field.key == 'actions' ? '50px' : 'auto' }">
+                    </template>
+                </TableResults>
             </div>
             <!-- Shown only we a customer was selected -->
             <div
+                id="pew-new-payment-form"
                 key="pew-selected-data"
                 v-else>
                 <b-button
@@ -31,9 +54,6 @@
                     @click="selectedData = null">
                     Volver
                 </b-button>
-                <b-form-group class="mb-0">
-                    <h5 class="subtitle subtitle-sub" md="4">Selecciona un periodo de pago</h5>
-                </b-form-group>
                 <NewPayment
                     :_id="selectedData._id"></NewPayment>
             </div>
@@ -45,13 +65,15 @@
     export default {
         data() {
             return {
+                searchResult: null, /* Is the result of a customer search */
                 selectedData: null, /* Stores the customer selected wich want to add a new payment */
                 tableFields: [
                     { key: 'active', label: 'Activo', sortable: true, class: 'text-center' },
                     { key: 'customerNumber', label: 'Nº Socio', sortable: true, class: 'text-center' },
-                    { key: 'name', label: 'Socio', sortable: true, },
+                    { key: 'name', label: 'Socio', sortable: true, class: 'text-nowrap' },
+                    { key: 'phones', label: 'Teléfonos', sortable: true, },
                     { key: 'dni', label: 'DNI', sortable: true, class: 'text-center' },
-                    { key: 'use', label: '', sortable: true, },
+                    { key: 'actions', label: '', sortable: true, },
                 ], /* Table fields */
                 wmodalpayments: null, /* Stores the imported wizard steps */
             }
@@ -72,3 +94,9 @@
         }
     }
 </script>
+<style scoped>
+    #pew-new-payment-form {
+        /* overflow-x: hidden; */
+        overflow-y: visible;
+    }
+</style>
