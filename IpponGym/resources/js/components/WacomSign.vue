44cdb96568450db242e5b94be54cb672b5cb92dc
Arturo <arzzz@hotmail.es>
2020-11-18 13:21:01 +0100

    Update WacomSign.vue

--- IpponGym/resources/js/components/WacomSign.vue
+++ IpponGym/resources/js/components/WacomSign.vue
@@ -243,8 +243,8 @@
             async xx() {
                 if (wizardEventController != undefined) {
                     await wizardEventController.body_onload();
-                    await start();
-                    // await wizardEventController.start_stop();
+                    // await start();
+                    await wizardEventController.start_stop(1);
                     this.capture();
                 }
             }
