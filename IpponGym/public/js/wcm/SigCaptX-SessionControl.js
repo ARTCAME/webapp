f307a3819f1da4502148c8d4186b02ce6559b5a7
Arturo <arzzz@hotmail.es>
2020-11-18 13:57:04 +0100

    Update SigCaptX-SessionControl.js

--- IpponGym/public/js/wcm/SigCaptX-SessionControl.js
+++ IpponGym/public/js/wcm/SigCaptX-SessionControl.js
@@ -18,7 +18,7 @@ var wizardEventController =
   {
     return new Promise((resolve, reject) => {
         clearTextBox();
-        await actionWhenRestarted();
+        actionWhenRestarted();
         resolve();
     })
   },
