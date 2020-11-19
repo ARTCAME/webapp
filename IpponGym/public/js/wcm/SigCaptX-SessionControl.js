f230525258845acdca63efd97cbb87b38a594d0c
Arturo <arzzz@hotmail.es>
2020-11-19 10:06:52 +0100

    Update SigCaptX-SessionControl.js

--- IpponGym/public/js/wcm/SigCaptX-SessionControl.js
+++ IpponGym/public/js/wcm/SigCaptX-SessionControl.js
@@ -12,243 +12,251 @@
 ***************************************************************************/
 
 /* wizardEventController is the main event handler for the wizard script */
-var wizardEventController =
-{
-  body_onload : function()
-  {
-    clearTextBox();
-    actionWhenRestarted();
-  },
-
-  start_stop : function(numScreens)
-  {
-    if( scriptIsRunning )
-    {
-      wizardEventController.stop();
-    }
-    else
-    {
-      wizardStart(numScreens);
-    }
-  },
-
-  stop : function()
-  {
-    if( !scriptIsRunning )
-    {
-      print("Script not running");
-    }
-    else
-    {
-      stopScript();
-    }
-  },
-
-  script_Completed : function(stopScriptNow)
-  {
-    print("Script completed");
-    if (stopScriptNow)
-    {
-      stopScript();
-    }
-    else
-    {
-      showSignature();
-    }
-  },
-
-  script_Cancelled : function()
-  {
-    print("Script cancelled");
-    wizardEventController.stop();
-  }
+async function body_onload() {
+        clearTextBox();
+        const x = await actionWhenRestarted();
+    return new Promise((resolve, reject) => {
+        resolve(x);
+    });
 }
+// var wizardEventController =
+// {
+//   body_onload : function()
+//   {
+//     clearTextBox();
+//     actionWhenRestarted();
+//   },
+
+//   start_stop : function(numScreens)
+//   {
+//     if( scriptIsRunning )
+//     {
+//       wizardEventController.stop();
+//     }
+//     else
+//     {
+//       wizardStart(numScreens);
+//     }
+//   },
+
+//   stop : function()
+//   {
+//     if( !scriptIsRunning )
+//     {
+//       print("Script not running");
+//     }
+//     else
+//     {
+//       stopScript();
+//     }
+//   },
+
+//   script_Completed : function(stopScriptNow)
+//   {
+//     print("Script completed");
+//     if (stopScriptNow)
+//     {
+//       stopScript();
+//     }
+//     else
+//     {
+//       showSignature();
+//     }
+//   },
+
+//   script_Cancelled : function()
+//   {
+//     print("Script cancelled");
+//     wizardEventController.stop();
+//   }
+// }
 
 // Function called to start off the wizard session
- function wizardStart(numScreens)
-{
-    console.log('wizardStart')
-  numScreenDisplays = numScreens;
+//  function wizardStart(numScreens)
+// {
+//     console.log('wizardStart')
+//   numScreenDisplays = numScreens;
 
-  if(!wgssSignatureSDK.running)
-  {
-    print("Session error. Restarting the session.");
-    actionWhenRestarted(window.WizardStart);
-    return;
-  }
+//   if(!wgssSignatureSDK.running)
+//   {
+//     print("Session error. Restarting the session.");
+//     actionWhenRestarted(window.WizardStart);
+//     return;
+//   }
 
-  if(null != wizCtl)
-  {
-    wizCtl.Close(onWizClose);
-    return;
-  }
-  start_wizard();
+//   if(null != wizCtl)
+//   {
+//     wizCtl.Close(onWizClose);
+//     return;
+//   }
+//   start_wizard();
 
-  function onWizClose(wizCtlV, status)
-  {
-    if(wgssSignatureSDK.ResponseStatus.INVALID_SESSION == status)
-    {
-      print("Session error. Restarting the session.");
-      actionWhenRestarted(window.WizardStart);
-    }
-    else
-    {
-      wizCtl = null;
-      start_wizard();
-    }
-  }
+//   function onWizClose(wizCtlV, status)
+//   {
+//     if(wgssSignatureSDK.ResponseStatus.INVALID_SESSION == status)
+//     {
+//       print("Session error. Restarting the session.");
+//       actionWhenRestarted(window.WizardStart);
+//     }
+//     else
+//     {
+//       wizCtl = null;
+//       start_wizard();
+//     }
+//   }
 
-  function start_wizard()
-  {
-    scriptIsRunning = true;
-    wizCtl = new wgssSignatureSDK.WizCtl(onWizCtlConstructor);
-  }
+//   function start_wizard()
+//   {
+//     scriptIsRunning = true;
+//     wizCtl = new wgssSignatureSDK.WizCtl(onWizCtlConstructor);
+//   }
 
-  function onWizCtlConstructor(wizCtlV, status)
-  {
-    if(wgssSignatureSDK.ResponseStatus.OK == status)
-    {
-      var visible = (true == document.getElementById("chkDisplayWizard").checked);
-      print("Enabling licence");
-      wizCtl.PutLicence(LICENCEKEY, onWizCtlPutLicence);
-    }
-    else
-    {
-      print("WizCtl Constructor error: " + status);
-      wizCtl = null;
-      if(wgssSignatureSDK.ResponseStatus.INVALID_SESSION == status)
-      {
-        print("Session error. Restarting the session.");
-        actionWhenRestarted(window.WizardStart);
-      }
-    }
-  }
+//   function onWizCtlConstructor(wizCtlV, status)
+//   {
+//     if(wgssSignatureSDK.ResponseStatus.OK == status)
+//     {
+//       var visible = (true == document.getElementById("chkDisplayWizard").checked);
+//       print("Enabling licence");
+//       wizCtl.PutLicence(LICENCEKEY, onWizCtlPutLicence);
+//     }
+//     else
+//     {
+//       print("WizCtl Constructor error: " + status);
+//       wizCtl = null;
+//       if(wgssSignatureSDK.ResponseStatus.INVALID_SESSION == status)
+//       {
+//         print("Session error. Restarting the session.");
+//         actionWhenRestarted(window.WizardStart);
+//       }
+//     }
+//   }
 
-  function onWizCtlPutLicence(sigCtlV, status)
-  {
-    if(callbackStatusOK("WizCtl PutLicence", status) == true)
-    {
-     //print("License set OK - now checking chkDisplayWizard");
-      var visible = (true == document.getElementById("chkDisplayWizard").checked);
-      wizCtl.PutVisibleWindow(visible, onPutVisibleWindow);
-    }
-  }
+//   function onWizCtlPutLicence(sigCtlV, status)
+//   {
+//     if(callbackStatusOK("WizCtl PutLicence", status) == true)
+//     {
+//      //print("License set OK - now checking chkDisplayWizard");
+//       var visible = (true == document.getElementById("chkDisplayWizard").checked);
+//       wizCtl.PutVisibleWindow(visible, onPutVisibleWindow);
+//     }
+//   }
 
-  function onPutVisibleWindow(wizCtlV, status)
-  {
-    if(callbackStatusOK("WizCtl PutVisibleWindow", status) == true)
-    {
-      wizCtl.PadConnect(onPadConnect);
-    }
-  }
+//   function onPutVisibleWindow(wizCtlV, status)
+//   {
+//     if(callbackStatusOK("WizCtl PutVisibleWindow", status) == true)
+//     {
+//       wizCtl.PadConnect(onPadConnect);
+//     }
+//   }
 
-  function onPadConnect(wizCtlV, connected, status)
-  {
-    if(wgssSignatureSDK.ResponseStatus.OK == status && connected)
-    {
-      wizCtl.GetPadWidth(onGetPadWidth);
-    }
-    else
-    {
-      print("Unable to make connection to the Pad. Check it is plugged in and try again.");
-      wizCtl.Close(onErrorClose);
-    }
-  }
+//   function onPadConnect(wizCtlV, connected, status)
+//   {
+//     if(wgssSignatureSDK.ResponseStatus.OK == status && connected)
+//     {
+//       wizCtl.GetPadWidth(onGetPadWidth);
+//     }
+//     else
+//     {
+//       print("Unable to make connection to the Pad. Check it is plugged in and try again.");
+//       wizCtl.Close(onErrorClose);
+//     }
+//   }
 
-  function onErrorClose(wizCtlG, status)
-  {
-    wizCtl = null;
-  }
+//   function onErrorClose(wizCtlG, status)
+//   {
+//     wizCtl = null;
+//   }
 
-  function onGetPadWidth(wizCtlV, padWidth, status)
-  {
-    if(callbackStatusOK("WizCtl GetPadWidth", status) == true)
-    {
-      width = padWidth;
-      wizCtl.GetPadHeight(onGetPadHeight);
-    }
-  }
+//   function onGetPadWidth(wizCtlV, padWidth, status)
+//   {
+//     if(callbackStatusOK("WizCtl GetPadWidth", status) == true)
+//     {
+//       width = padWidth;
+//       wizCtl.GetPadHeight(onGetPadHeight);
+//     }
+//   }
 
-  function onGetPadHeight(wizCtlV, padHeight, status)
-  {
-    var buttonTextSource;
-    var chkBoxSize;
+//   function onGetPadHeight(wizCtlV, padHeight, status)
+//   {
+//     var buttonTextSource;
+//     var chkBoxSize;
 
-    if(wgssSignatureSDK.ResponseStatus.OK == status)
-    {
-      height = padHeight;
+//     if(wgssSignatureSDK.ResponseStatus.OK == status)
+//     {
+//       height = padHeight;
 
-      /* If the user wants a large check box pass this as a parameter to CPad_STU in SigCaptX-Wizard-PadDefs.js */
-      /* Note that this code is also used by the PIN pad sample which doesn't have the user options so make sure
-         the tags exist first */
+//       /* If the user wants a large check box pass this as a parameter to CPad_STU in SigCaptX-Wizard-PadDefs.js */
+//       /* Note that this code is also used by the PIN pad sample which doesn't have the user options so make sure
+//          the tags exist first */
 
-      if (document.getElementById("chkLargeCheckbox") != null)
-      {
-        if (document.getElementById("chkLargeCheckbox").checked)
-        {
-          chkBoxSize = checkSizeSelection.LARGE;
-        }
-        else
-        {
-          chkBoxSize = checkSizeSelection.STANDARD;
-        }
-      }
+//       if (document.getElementById("chkLargeCheckbox") != null)
+//       {
+//         if (document.getElementById("chkLargeCheckbox").checked)
+//         {
+//           chkBoxSize = checkSizeSelection.LARGE;
+//         }
+//         else
+//         {
+//           chkBoxSize = checkSizeSelection.STANDARD;
+//         }
+//       }
 
-      pad = new CPad_STU(width, height, chkBoxSize);
+//       pad = new CPad_STU(width, height, chkBoxSize);
 
-      /* Decide what parameter to pass to Screen1() in SigCaptX-WizardCheckbox-PadDefs.js
-         This depends on what the user has selected on the HTML form. Make sure the tags exist first */
+//       /* Decide what parameter to pass to Screen1() in SigCaptX-WizardCheckbox-PadDefs.js
+//          This depends on what the user has selected on the HTML form. Make sure the tags exist first */
 
-      if (document.getElementById("utf8") != null)
-      {
-        if (document.getElementById("utf8").checked)
-        {
-          buttonTextSource = textSource.UTF8;
-        }
-        else
-        {
-          if (document.getElementById("local").checked)
-          {
-            buttonTextSource = textSource.LOCAL;
-          }
-          else
-          {
-            if (document.getElementById("remote").checked)
-            {
-              buttonTextSource = textSource.REMOTE;
-            }
-            else
-            {
-              buttonTextSource = textSource.STANDARD;
-            }
-          }
-        }
-      }
-      //print("Calling screen1 with button source " + buttonTextSource);
-      display_1 = new screen_Display1(pad, buttonTextSource);
-      if (numScreenDisplays >= 2)
-      {
-        display_2 = new screen_Display2(pad, buttonTextSource);
-      }
-      if (numScreenDisplays >= 3)
-      {
-        display_3 = new screen_Display3(pad, buttonTextSource);
-      }
-     //print("Pad type: " + pad.Type);
-      step1();
-    }
-    else
-    {
-      print("WizCtl PutVisibleWindow error: " + status);
-    }
-  }
-}
+//       if (document.getElementById("utf8") != null)
+//       {
+//         if (document.getElementById("utf8").checked)
+//         {
+//           buttonTextSource = textSource.UTF8;
+//         }
+//         else
+//         {
+//           if (document.getElementById("local").checked)
+//           {
+//             buttonTextSource = textSource.LOCAL;
+//           }
+//           else
+//           {
+//             if (document.getElementById("remote").checked)
+//             {
+//               buttonTextSource = textSource.REMOTE;
+//             }
+//             else
+//             {
+//               buttonTextSource = textSource.STANDARD;
+//             }
+//           }
+//         }
+//       }
+//       //print("Calling screen1 with button source " + buttonTextSource);
+//       display_1 = new screen_Display1(pad, buttonTextSource);
+//       if (numScreenDisplays >= 2)
+//       {
+//         display_2 = new screen_Display2(pad, buttonTextSource);
+//       }
+//       if (numScreenDisplays >= 3)
+//       {
+//         display_3 = new screen_Display3(pad, buttonTextSource);
+//       }
+//      //print("Pad type: " + pad.Type);
+//       step1();
+//     }
+//     else
+//     {
+//       print("WizCtl PutVisibleWindow error: " + status);
+//     }
+//   }
+// }
 
 
 /* This function is called if connection with the SigCaptX service has to be re-initiated because for whatever reason it has stopped or failed */
 function actionWhenRestarted(callback)
 {
-  wgssSignatureSDK = null;
+  return new Promise((resolve, reject) => {
+    wgssSignatureSDK = null;
   sigObj = null;
   sigCtl = null;
   dynCapt = null;
@@ -387,12 +395,14 @@ function actionWhenRestarted(callback)
     if(wgssSignatureSDK.ResponseStatus.OK == status)
     {
       print("DLL: flSigCapt.dll v" + property.text);
-      print("Test application ready.");
-    //   print("Press 'Capture' or 'Start Wizard' to capture a signature.");
+    //   print("Test application ready.");
+    print("Tablet ready");
+      //   print("Press 'Capture' or 'Start Wizard' to capture a signature.");
       if('function' === typeof callback)
       {
         callback();
       }
+      resolve(wgssSignatureSDK);
     }
     else
     {
@@ -413,41 +423,44 @@ function actionWhenRestarted(callback)
   {
     //We don't check the status for compatibility with older version
     //Older versions gave an error status when closing an idle WizCtl
-    print("Test application ready.");
+    // print("Test application ready.");
+    print("Tablet ready");
     // print("Press 'Start Wizard' to capture a signature.");
     if('function' === typeof callback)
     {
       callback();
     }
+    resolve(wgssSignatureSDK);
   }
+})
 }
 
-// Function called to stop the wizard script
-function stopScript()
-{
-  scriptIsRunning = false;
-//   document.getElementById("btnStartStopWizard").value = "Start Wizard";
-  if(null != wizCtl)
-  {
-    wizCtl.Reset(onReset);
-  }
+// // Function called to stop the wizard script
+// function stopScript()
+// {
+//   scriptIsRunning = false;
+// //   document.getElementById("btnStartStopWizard").value = "Start Wizard";
+//   if(null != wizCtl)
+//   {
+//     wizCtl.Reset(onReset);
+//   }
 
-  function onReset(wizCtlV, status)
-  {
-    if(callbackStatusOK("WizCtl Reset", status))
-    {
-      wizCtl.Close(onClose);
-    }
-  }
+//   function onReset(wizCtlV, status)
+//   {
+//     if(callbackStatusOK("WizCtl Reset", status))
+//     {
+//       wizCtl.Close(onClose);
+//     }
+//   }
 
-  function onClose(wizCtlV, status)
-  {
-    if(callbackStatusOK("WizCtl PadDisconnect", status))
-    {
-      wizCtl = null;
-      print("Pad disconnected");
-    }
-  }
-}
+//   function onClose(wizCtlV, status)
+//   {
+//     if(callbackStatusOK("WizCtl PadDisconnect", status))
+//     {
+//       wizCtl = null;
+//       print("Pad disconnected");
+//     }
+//   }
+// }
 
 
