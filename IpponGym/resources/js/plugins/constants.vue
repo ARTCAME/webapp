b2422741e3a19375325f79e4f3663f22ffdcea6a
Arturo <arzzz@hotmail.es>
2020-11-23 18:28:06 +0100

    Create constants.vue

---
+++ IpponGym/resources/js/plugins/constants.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        install (Vue, options) {
+            /**
+             * Change the text of an element
+             *
+             * @param {Array} id: The id of the elements to change
+             */
+            Vue.prototype.$constants = {
+            }
+        }
+    }
+</script>
