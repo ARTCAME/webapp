6e92189ab8e8842839b79c15578501408e796a82
Arturo <arzzz@hotmail.es>
2020-11-23 18:24:07 +0100

    Create ButtonIcon.vue

---
+++ IpponGym/resources/js/components/ButtonIcon.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-button
+        v-bind="$attrs"
+        @click="$emit('click')">
+        <b-spinner
+            small
+            type="grow"
+            v-if="spin"></b-spinner>
+        <fa-icon
+            v-else-if="icon"
+            :icon="icon"></fa-icon>
+        <span
+            class="ml-2"
+            v-if="!noText">
+            <slot></slot>
+        </span>
+    </b-button>
+</template>
+<script>
+    export default {
+        inheritAttrs: false,
+        props: {
+            icon: String,
+            noText: Boolean,
+            spin: Boolean,
+        }
+    }
+</script>
+<style scoped>
+    /* Sets the btn-sm font-size */
+    .btn-sm *,
+    .btn-group-sm > .btn * {
+        font-size: 0.875rem!important;
+    }
+</style>
