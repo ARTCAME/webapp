b8befa39e2b31e97068e274a7fc057d33935ffcc
Arturo <arzzz@hotmail.es>
2021-01-04 15:02:57 +0100

    Update DocUtilsDocumentos.vue

--- IpponGym/resources/js/components/userdocassets/DocUtilsDocumentos.vue
+++ IpponGym/resources/js/components/userdocassets/DocUtilsDocumentos.vue
@@ -1,40 +1,39 @@
 <template>
-    <div id="doc-utils-documentos">
-        <b-form-group>
-            <h1 class="subtitle">
-                Genera y descarga documentos de socio
-            </h1>
-            <p>
-                Desde esta utilidad podremos descargar manualmente documentos para entregar a nuestro socio/a o simplemente para guardarlos.
-            </p>
-            <p>
-                Com veremos, estos documentos se podrán descargar desde otros apartados de la aplicación pero aquí se da un acceso unificado a todos los documentos descargables.
-            </p>
-            <DocImg alt="Ubicación del generador de documentos en home" desc="Ubicación del generador de documentos en la página de bienvenida" img="Documentos1.png" type="image/png"></DocImg>
-            <DocImg alt="Ubicación del generador de documentos en barra de navegación" desc="Ubicación del generador de documentos en la barra de navegación" img="Documentos2.png" type="image/png"></DocImg>
-            <p>
-                Los documentos que vamos a poder descargar son:
-                <br>
-                &emsp;- Recibo: Podremos escoger de entre los pagos que tenga un socio confirmados
-                <br>
-                &emsp;- Consentimiento expreso: Es el consentimiento al tratamiento de datos del socio. Si el socio es menor se reflejará en el documento junto con los datos del tutor incluidos en la ficha del socio
-                <br>
-                &emsp;- Cesión de imagen: Es la confirmación de si el socio nos permite o no usar su imagen en diferentes medios. Si el socio es menor se reflejará en el documento junto con los datos del tutor incluidos en la ficha del socio
-                <br>
-            </p>
-            <p>
-                Estos documentos no siempre estarán habilitados y deberemos tener en cuenta ciertas consideraciones:
-                <br>
-                &emsp;- Si un socio no tiene ningún pago con el estado 'Confirmado' no se podrá generar ningún recibo, no se pueden generar recibos en estado 'Pendiente' o 'Devuelto'
-                <br>
-                &emsp;- Si un socio no ha aceptado las políticas legales de IPPONGYM no podremos imprimir estos documentos y las opciones nos aparecerán deshabilitadas
-                <br>
-            </p>
-            <DocImg alt="Selector de documentos y buscador de pagos" desc="Selector de documentos y buscador de pagos" img="Documentos3.png" type="image/png"></DocImg>
-            <p>
-                Una vez confirmado el tipo de documento a descargar podremos ver el resumen de datos que lo compondrán y si nos falta alguno de ellos seremos advertidos aunque esto no evitará que podamos descargar el documento aunque esté incompleto.
-            </p>
-            <DocImg alt="Resumen de datos para el documento" desc="Resumen de datos para el documento" img="Documentos4.png" type="image/png"></DocImg>
-        </b-form-group>
-    </div>
+    <section id="doc-utils-documentos">
+        <h1 class="subtitle">
+            Genera y descarga documentos de socio
+        </h1>
+        <small class="text-muted">Última actualización 2020-01-04</small>
+        <p>
+            Desde esta utilidad podremos descargar manualmente documentos para entregar a nuestro socio/a o simplemente para guardarlos.
+        </p>
+        <p>
+            Com veremos, estos documentos se podrán descargar desde otros apartados de la aplicación pero aquí se da un acceso unificado a todos los documentos descargables.
+        </p>
+        <DocImg alt="Ubicación del generador de documentos en home" desc="Ubicación del generador de documentos en la página de bienvenida" img="Documentos1.png" type="image/png"></DocImg>
+        <DocImg alt="Ubicación del generador de documentos en barra de navegación" desc="Ubicación del generador de documentos en la barra de navegación" img="Documentos2.png" type="image/png"></DocImg>
+        <p>
+            Los documentos que vamos a poder descargar son:
+            <br>
+            &emsp;- Recibos: Podremos escoger de entre los pagos confirmados que tenga un socio
+            <br>
+            &emsp;- Consentimiento expreso: Es el consentimiento al tratamiento de datos del socio. Si el socio es menor se reflejará en el documento junto con los datos del tutor incluidos en la ficha del socio
+            <br>
+            &emsp;- Cesión de imagen: Es la confirmación de si el socio nos permite o no usar su imagen en medios digitales o analógicos. Si el socio es menor se reflejará en el documento junto con los datos del tutor incluidos en la ficha del socio
+            <br>
+        </p>
+        <p>
+            Estos documentos no siempre estarán habilitados y deberemos tener en cuenta ciertas consideraciones:
+            <br>
+            &emsp;- Si un socio no tiene ningún pago con el estado 'Confirmado' no se podrá generar ningún recibo, no se pueden generar recibos sobre pagos en estado 'Pendiente' o 'Devuelto'
+            <br>
+            &emsp;- Si un socio no ha aceptado las políticas legales de IPPONGYM no podremos imprimir estos documentos y las opciones nos aparecerán deshabilitadas
+            <br>
+        </p>
+        <DocImg alt="Selector de documentos y buscador de pagos" desc="Selector de documentos y buscador de pagos" img="Documentos3.png" type="image/png"></DocImg>
+        <p>
+            Una vez confirmado el tipo de documento a descargar podremos ver el resumen de datos que lo compondrán y si nos falta alguno de ellos seremos advertidos aunque esto no evitará que podamos descargar el documento aunque esté incompleto.
+        </p>
+        <DocImg alt="Resumen de datos para el documento" desc="Resumen de datos para el documento" img="Documentos4.png" type="image/png"></DocImg>
+    </section>
 </template>
